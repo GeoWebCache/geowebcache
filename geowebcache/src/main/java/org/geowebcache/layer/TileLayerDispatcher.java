ce3faa333645f47c07ad4c2b33a891efce0b3ef7
Arne Kepp <arne@tiledmarble.org>
2008-04-10 21:43:22 +0000

    Temporary commit for transport
    git-svn-id: http://geowebcache.org/svn/trunk@129 e7b91dd5-889a-44ae-8e97-0abfc27e49b2

--- geowebcache/src/main/java/org/geowebcache/layer/TileLayerDispatcher.java
+++ geowebcache/src/main/java/org/geowebcache/layer/TileLayerDispatcher.java
@@ -19,6 +19,7 @@ package org.geowebcache.layer;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -26,48 +27,74 @@ import org.geowebcache.GeoWebCacheException;
 import org.geowebcache.util.Configuration;
 
 public class TileLayerDispatcher {
-	private static Log log = LogFactory.getLog(org.geowebcache.layer.TileLayerDispatcher.class);
-	
-	private HashMap layers = null;
-	
-	private Configuration config = null;
-	
-	public TileLayerDispatcher() {
-		log.info("TileLayerFactor constructed");
-	}
-	
-	public TileLayer getTileLayer(String layerIdent) {
-		if(this.layers == null) {
-			initLayers();
-		}
-		
-		return (TileLayer) layers.get(layerIdent);
-	}
+    private static Log log = LogFactory
+            .getLog(org.geowebcache.layer.TileLayerDispatcher.class);
 
-	public void setConfig(Configuration config) {
-		this.config = config;
-	}
-	
-	public void initLayers() {
-		this.layers = new HashMap();
-		
-		Map configLayers = null;
-		try {
-			configLayers = config.getTileLayers();
-		} catch (GeoWebCacheException gwce) {
-		    log.error(gwce.getMessage());
-                    log.error("Failed to add layers from " + config.getIdentifier());
-		}
-		
-		log.info("Adding layers from " + config.getIdentifier());
-		if(configLayers != null && configLayers.size() > 0 ){
-			Iterator iter = configLayers.keySet().iterator();
-			while(iter.hasNext()) {
-				String layerIdent = (String) iter.next();
-				layers.put(layerIdent, configLayers.get(layerIdent));
-			}
-		} else {
-			log.error("Configuration "+config.getIdentifier()+" contained no layers.");
-		}
-	}
+    private HashMap layers = null;
+
+    private Configuration config = null;
+
+    private boolean isInitialized = false;
+
+    private final ReentrantLock initLock = new ReentrantLock();
+
+    public TileLayerDispatcher() {
+        log.info("TileLayerFactor constructed");
+    }
+
+    public TileLayer getTileLayer(String layerIdent) {
+        if (!this.isInitialized) {
+            initLayers();
+        }
+
+        return (TileLayer) layers.get(layerIdent);
+    }
+
+    public void setConfig(Configuration config) {
+        this.config = config;
+    }
+
+    public void initLayers() {
+        initLock.lock(); // block until condition holds
+        try {
+            if (this.layers != null) {
+                System.out
+                        .println("BAH, layers have already been initialized.");
+                return;
+            } else {
+                System.out.println("WOOOT! I GET TO INITIALIZE. LUCKY ME.");
+            }
+
+            this.layers = new HashMap();
+
+            Map configLayers = null;
+            try {
+                configLayers = config.getTileLayers();
+            } catch (GeoWebCacheException gwce) {
+                log.error(gwce.getMessage());
+                log
+                        .error("Failed to add layers from "
+                                + config.getIdentifier());
+            }
+
+            log.info("Adding layers from " + config.getIdentifier());
+            if (configLayers != null && configLayers.size() > 0) {
+                Iterator iter = configLayers.keySet().iterator();
+                while (iter.hasNext()) {
+                    String layerIdent = (String) iter.next();
+                    layers.put(layerIdent, configLayers.get(layerIdent));
+                }
+            } else {
+                log.error("Configuration " + config.getIdentifier()
+                        + " contained no layers.");
+            }
+
+            // This is a hack
+            if (layers != null) {
+                this.isInitialized = true;
+            }
+        } finally {
+            initLock.unlock();
+        }
+    }
 }
