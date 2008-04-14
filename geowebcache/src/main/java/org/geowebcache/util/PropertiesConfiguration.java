f6003a4b875954481e28087f991033ed32fb588b
Arne Kepp <arne@tiledmarble.org>
2008-04-14 17:41:16 +0000

    Formatting cleanup, no functional changes
    git-svn-id: http://geowebcache.org/svn/trunk@136 e7b91dd5-889a-44ae-8e97-0abfc27e49b2

--- geowebcache/src/main/java/org/geowebcache/util/PropertiesConfiguration.java
+++ geowebcache/src/main/java/org/geowebcache/util/PropertiesConfiguration.java
@@ -37,160 +37,160 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.ServletContextResource;
 
 public class PropertiesConfiguration implements Configuration,
-		ApplicationContextAware {
-	private static Log log = LogFactory
-			.getLog(org.geowebcache.util.PropertiesConfiguration.class);
+        ApplicationContextAware {
+    private static Log log = LogFactory
+            .getLog(org.geowebcache.util.PropertiesConfiguration.class);
 
-	private String absPath = null;
+    private String absPath = null;
 
-	private String relPath = null;
+    private String relPath = null;
 
-	private File configDirH = null;
+    private File configDirH = null;
 
-	private WebApplicationContext context;
+    private WebApplicationContext context;
 
-	private CacheFactory cacheFactory = null;
+    private CacheFactory cacheFactory = null;
 
-	public PropertiesConfiguration(CacheFactory cacheFactory) {
-		this.cacheFactory = cacheFactory;
-	}
+    public PropertiesConfiguration(CacheFactory cacheFactory) {
+        this.cacheFactory = cacheFactory;
+    }
 
-	public Map getTileLayers() throws GeoWebCacheException {
-		if (configDirH == null) {
-			determineConfigDirH();
-		}
+    public Map getTileLayers() throws GeoWebCacheException {
+        if (configDirH == null) {
+            determineConfigDirH();
+        }
 
-		// Find all the property files and process each one into a TileLayer
-		File[] propFiles = findPropFiles(configDirH);
+        // Find all the property files and process each one into a TileLayer
+        File[] propFiles = findPropFiles(configDirH);
 
-		if (propFiles != null && propFiles.length > 0) {
-			log.trace("Found " + propFiles.length + " property files in "
-					+ configDirH.getAbsolutePath());
-		} else {
-			log.error("Found no property files in "
-					+ configDirH.getAbsolutePath());
-			return null;
-		}
+        if (propFiles != null && propFiles.length > 0) {
+            log.trace("Found " + propFiles.length + " property files in "
+                    + configDirH.getAbsolutePath());
+        } else {
+            log.error("Found no property files in "
+                    + configDirH.getAbsolutePath());
+            return null;
+        }
 
-		HashMap layers = new HashMap();
+        HashMap layers = new HashMap();
 
-		// Loop over the property files, create TileLayers
-		for (int i = 0; i < propFiles.length; i++) {
-			Properties props = readProperties(propFiles[i]);
-			if (props == null) {
-				continue;
-			}
+        // Loop over the property files, create TileLayers
+        for (int i = 0; i < propFiles.length; i++) {
+            Properties props = readProperties(propFiles[i]);
+            if (props == null) {
+                continue;
+            }
 
-			String layerName = propFiles[i].getName();
-			String[] nameComps = layerName.split("\\.");
-			layerName = new String(nameComps[0].substring(6));
+            String layerName = propFiles[i].getName();
+            String[] nameComps = layerName.split("\\.");
+            layerName = new String(nameComps[0].substring(6));
 
-			log.trace("Adding layer " + layerName);
+            log.trace("Adding layer " + layerName);
 
-			// TODO need support for other types of layers
-			WMSLayer layer = new WMSLayer(layerName, props, cacheFactory);
+            // TODO need support for other types of layers
+            WMSLayer layer = new WMSLayer(layerName, props, cacheFactory);
 
-			if (layer != null) {
-				layers.put(layerName, layer);
-			}
-		}
-		return layers;
-	}
+            if (layer != null) {
+                layers.put(layerName, layer);
+            }
+        }
+        return layers;
+    }
 
-	public void determineConfigDirH() {
-		if (absPath != null) {
-			configDirH = new File(absPath);
-			return;
-		}
+    public void determineConfigDirH() {
+        if (absPath != null) {
+            configDirH = new File(absPath);
+            return;
+        }
 
-		if (relPath == null) {
-			log
-					.warn("No configuration directory was specified, reverting to default: ");
-			relPath = ".";
-		}
+        if (relPath == null) {
+            log
+                    .warn("No configuration directory was specified, reverting to default: ");
+            relPath = ".";
+        }
 
-		ServletContextResource servResource = new ServletContextResource(
-				context.getServletContext(), relPath);
+        ServletContextResource servResource = new ServletContextResource(
+                context.getServletContext(), relPath);
 
-		try {
-			configDirH = servResource.getFile();
-		} catch (IOException ioe) {
-			log.error(ioe.getMessage());
-			ioe.printStackTrace();
-			return;
-		}
-		log.warn("Configuration directory set to: "
-				+ configDirH.getAbsolutePath());
+        try {
+            configDirH = servResource.getFile();
+        } catch (IOException ioe) {
+            log.error(ioe.getMessage());
+            ioe.printStackTrace();
+            return;
+        }
+        log.warn("Configuration directory set to: "
+                + configDirH.getAbsolutePath());
 
-		if (!configDirH.exists() || !configDirH.canRead()) {
-			log.error(configDirH.getAbsoluteFile()
-					+ " cannot be read or does not exist!");
-		}
-	}
+        if (!configDirH.exists() || !configDirH.canRead()) {
+            log.error(configDirH.getAbsoluteFile()
+                    + " cannot be read or does not exist!");
+        }
+    }
 
-	/**
-	 * Find all the .properties files in the configuration file directory.
-	 * 
-	 * @param configDirH
-	 */
-	private File[] findPropFiles(File configDirH) {
-		FilenameFilter select = new ExtensionFileListFilter("layer_",
-				"properties");
-		return configDirH.listFiles(select);
-	}
+    /**
+     * Find all the .properties files in the configuration file directory.
+     * 
+     * @param configDirH
+     */
+    private File[] findPropFiles(File configDirH) {
+        FilenameFilter select = new ExtensionFileListFilter("layer_",
+                "properties");
+        return configDirH.listFiles(select);
+    }
 
-	private Properties readProperties(File propFile) {
-		log.debug("Found " + propFile.getAbsolutePath() + " file.");
-		Properties props = null;
-		try {
-			FileInputStream in = new FileInputStream(propFile);
-			props = new Properties();
-			props.load(in);
-			in.close();
-		} catch (IOException ioe) {
-			log.error("Could not read " + propFile.getAbsolutePath()
-					+ " file: ", ioe);
-		}
-		return props;
-	}
+    private Properties readProperties(File propFile) {
+        log.debug("Found " + propFile.getAbsolutePath() + " file.");
+        Properties props = null;
+        try {
+            FileInputStream in = new FileInputStream(propFile);
+            props = new Properties();
+            props.load(in);
+            in.close();
+        } catch (IOException ioe) {
+            log.error("Could not read " + propFile.getAbsolutePath()
+                    + " file: ", ioe);
+        }
+        return props;
+    }
 
-	public String getIdentifier() {
-		return configDirH.getAbsolutePath();
-	}
+    public String getIdentifier() {
+        return configDirH.getAbsolutePath();
+    }
 
-	public void setRelativePath(String relPath) {
-		this.relPath = relPath;
-	}
+    public void setRelativePath(String relPath) {
+        this.relPath = relPath;
+    }
 
-	public void setAbsolutePath(String absPath) {
-		this.absPath = absPath;
-	}
+    public void setAbsolutePath(String absPath) {
+        this.absPath = absPath;
+    }
 
-	public void setApplicationContext(ApplicationContext arg0)
-			throws BeansException {
-		context = (WebApplicationContext) arg0;
-	}
+    public void setApplicationContext(ApplicationContext arg0)
+            throws BeansException {
+        context = (WebApplicationContext) arg0;
+    }
 }
 
 class ExtensionFileListFilter implements FilenameFilter {
-	private String prefix;
+    private String prefix;
 
-	private String extension;
+    private String extension;
 
-	public ExtensionFileListFilter(String prefix, String extension) {
-		this.prefix = prefix;
-		this.extension = extension;
-	}
+    public ExtensionFileListFilter(String prefix, String extension) {
+        this.prefix = prefix;
+        this.extension = extension;
+    }
 
-	public boolean accept(File directory, String filename) {
-		if (prefix != null && extension != null) {
-			System.out.println(" properties: " + directory.getAbsolutePath()
-					+ " " + filename);
-			boolean ret = filename.endsWith('.' + extension)
-					&& filename.startsWith(prefix);
-			return ret;
-		} else {
-			return false;
-		}
-	}
+    public boolean accept(File directory, String filename) {
+        if (prefix != null && extension != null) {
+            System.out.println(" properties: " + directory.getAbsolutePath()
+                    + " " + filename);
+            boolean ret = filename.endsWith('.' + extension)
+                    && filename.startsWith(prefix);
+            return ret;
+        } else {
+            return false;
+        }
+    }
 }
