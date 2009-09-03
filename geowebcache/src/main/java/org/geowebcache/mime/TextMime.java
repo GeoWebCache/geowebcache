a81ee606efb4ebc910667085f34f09e9f567fc44
Arne Kepp <arne@tiledmarble.org>
2009-09-03 12:24:43 +0000

    Clean up text MIME types
    git-svn-id: http://geowebcache.org/svn/trunk@743 e7b91dd5-889a-44ae-8e97-0abfc27e49b2

--- geowebcache/src/main/java/org/geowebcache/mime/TextMime.java
+++ geowebcache/src/main/java/org/geowebcache/mime/TextMime.java
@@ -6,21 +6,32 @@ public class TextMime extends MimeType {
        "text/plain", "txt", "txt",
        "text/plain", true);
     
+    public static final TextMime txtHtml = new TextMime(
+            "text/html", "txt.html", "html",
+            "text/html", true);
+        
     private TextMime(String mimeType, String fileExtension, 
             String internalName, String format, boolean noop) {
         super(mimeType, fileExtension, internalName, format, false);
     }
     
     protected static TextMime checkForFormat(String formatStr) throws MimeException {
-        if(formatStr.equalsIgnoreCase("text/plain")) {
-            return txt;
+        if(formatStr.toLowerCase().startsWith("text")) {
+            if(formatStr.equalsIgnoreCase("text/plain")) {
+                return txt;
+            } else if(formatStr.startsWith("text/html")) {
+                return txtHtml;
+            }
         }
+        
         return null;
     }
     
     protected static TextMime checkForExtension(String fileExtension) throws MimeException {
         if(fileExtension.equalsIgnoreCase("txt")) {
             return txt;
+        } else if(fileExtension.equalsIgnoreCase("txt.html")) {
+            return txtHtml;
         }
         
         return null;
