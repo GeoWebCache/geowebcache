c6782205e89a95c245d5b465105aefa4a6229608
Arne Kepp <arne@tiledmarble.org>
2009-12-04 13:48:18 +0000

    1.2.1 instead of Nightly
    git-svn-id: http://geowebcache.org/svn/tags/1.2.1@851 e7b91dd5-889a-44ae-8e97-0abfc27e49b2

--- geowebcache/src/main/java/org/geowebcache/GeoWebCacheDispatcher.java
+++ geowebcache/src/main/java/org/geowebcache/GeoWebCacheDispatcher.java
@@ -420,7 +420,7 @@ public class GeoWebCacheDispatcher extends AbstractController {
         StringBuilder str = new StringBuilder();
         
         str.append("<html>\n"+ServletUtils.gwcHtmlHeader("GWC Home") +"<body>\n" + ServletUtils.gwcHtmlLogoLink(baseUrl));
-        str.append("<h3>Welcome to GeoWebCache version NIGHTLY, built 2009-12-04</h3>\n");
+        str.append("<h3>Welcome to GeoWebCache version 1.2.1, built 2009-12-04</h3>\n");
         str.append("<p><a href=\"http://geowebcache.org\">GeoWebCache</a> is an advanced tile cache for WMS servers.");
         str.append("It supports a large variety of protocols and formats, including WMS-C, WMTS, KML, Google Maps and Virtual Earth.</p>");
         str.append("<h3>Automatically Generated Demos:</h3>\n");
