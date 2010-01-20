71f70c6fcca73ffb03aef0b639409c806fbd2a02
Gabriel Roldan <gabriel.roldan@gmail.com>
2010-01-20 13:36:52 +0000

    new unit test suite (only one test for seeding so far) for WMSLayer, introduces mockrunner and easymock unit testing libraries
    git-svn-id: http://geowebcache.org/svn/trunk@871 e7b91dd5-889a-44ae-8e97-0abfc27e49b2

---
+++ geowebcache/src/test/java/org/geowebcache/layer/wms/WMSLayerTest.java
@@ -0,0 +1,140 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+package org.geowebcache.layer.wms;
+
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.replay;
+import static org.easymock.classextension.EasyMock.verify;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import junit.framework.TestCase;
+
+import org.easymock.Capture;
+import org.easymock.classextension.EasyMock;
+import org.geowebcache.conveyor.ConveyorTile;
+import org.geowebcache.grid.BoundingBox;
+import org.geowebcache.grid.GridSet;
+import org.geowebcache.grid.GridSetBroker;
+import org.geowebcache.grid.GridSubset;
+import org.geowebcache.grid.GridSubsetFactory;
+import org.geowebcache.mime.MimeType;
+import org.geowebcache.storage.StorageBroker;
+import org.geowebcache.storage.TileObject;
+
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.mockrunner.mock.web.MockHttpServletResponse;
+
+/**
+ * Unit test suite for {@link WMSLayer}
+ * 
+ * @author Gabriel Roldan (OpenGeo)
+ * @version $Id$
+ */
+public class WMSLayerTest extends TestCase {
+
+    private final GridSetBroker gridSetBroker = new GridSetBroker(false, false);
+
+    public void testSeed() throws Exception {
+        WMSLayer layer = createWMSLayer();
+
+        WMSSourceHelper mockSourceHelper = EasyMock.createMock(WMSSourceHelper.class);
+        byte[] returnBytes = createFakeSourceImage(layer);
+        expect(mockSourceHelper.makeRequest((WMSMetaTile) anyObject())).andReturn(returnBytes);
+        replay(mockSourceHelper);
+
+        layer.setSourceHelper(mockSourceHelper);
+
+        final StorageBroker mockStorageBroker = EasyMock.createMock(StorageBroker.class);
+        Capture<TileObject> captured = new Capture<TileObject>();
+        expect(mockStorageBroker.put(EasyMock.capture(captured))).andReturn(true).anyTimes();
+        replay(mockStorageBroker);
+
+        String layerId = layer.getName();
+        HttpServletRequest servletReq = new MockHttpServletRequest();
+        HttpServletResponse servletResp = new MockHttpServletResponse();
+
+        long[] gridLoc = { 0, 0, 0 };// x, y, level
+        MimeType mimeType = layer.getMimeTypes().get(0);
+        GridSet gridSet = gridSetBroker.WORLD_EPSG4326;
+        String gridSetId = gridSet.getName();
+        ConveyorTile tile = new ConveyorTile(mockStorageBroker, layerId, gridSetId, gridLoc,
+                mimeType, null, null, servletReq, servletResp);
+
+        boolean tryCache = false;
+        layer.seedTile(tile, tryCache);
+
+        assertEquals(1, captured.getValues().size());
+        TileObject value = captured.getValue();
+        assertNotNull(value);
+        assertEquals("image/png", value.getBlobFormat());
+        assertNotNull(value.getBlob());
+        BufferedImage read = ImageIO.read(new ByteArrayInputStream(value.getBlob()));
+        assertNotNull(read);
+
+        verify(mockSourceHelper);
+        verify(mockStorageBroker);
+    }
+
+    private byte[] createFakeSourceImage(final WMSLayer layer) throws IOException {
+
+        int tileWidth = layer.getGridSubset(gridSetBroker.WORLD_EPSG4326.getName()).getGridSet()
+                .getTileWidth();
+        int tileHeight = layer.getGridSubset(gridSetBroker.WORLD_EPSG4326.getName()).getGridSet()
+                .getTileHeight();
+
+        int width = tileWidth * layer.getMetaTilingFactors()[0];
+        int height = tileHeight * layer.getMetaTilingFactors()[1];
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        RenderedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        String formatName = layer.getMimeTypes().get(0).getInternalName();
+        ImageIO.write(image, formatName, out);
+        return out.toByteArray();
+    }
+
+    private WMSLayer createWMSLayer() {
+
+        String[] urls = { "http://localhost:38080/wms" };
+        List<String> formatList = Collections.singletonList("image/png");
+
+        Hashtable<String, GridSubset> grids = new Hashtable<String, GridSubset>();
+
+        GridSubset grid = GridSubsetFactory.createGridSubSet(gridSetBroker.WORLD_EPSG4326,
+                new BoundingBox(-30.0, 15.0, 45.0, 30), 0, 10);
+
+        grids.put(grid.getName(), grid);
+        int[] metaWidthHeight = { 3, 3 };
+
+        WMSLayer layer = new WMSLayer("test:layer", urls, "aStyle", "test:layer", formatList,
+                grids, metaWidthHeight, "vendorparam=true", false);
+
+        layer.initialize(gridSetBroker);
+
+        return layer;
+    }
+
+}
