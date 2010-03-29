83f1d568c042e735ef40f877ad439ba0b7bce808
Arne Kepp <arne@tiledmarble.org>
2010-03-29 23:13:46 +0000

    Updating nightly skip post modularization
    git-svn-id: http://geowebcache.org/svn/trunk@1024 e7b91dd5-889a-44ae-8e97-0abfc27e49b2

--- geowebcache/tools/nightly.sh
+++ geowebcache/tools/nightly.sh
@@ -2,23 +2,25 @@
 echo "This script is very purpose-specific, please edit the source to enable it for your particular environment"
 exit 0
 
+cd /home/ak/nightly
+
 DATE=`date +%Y%m%d`
 NICEDATE=`date +%Y-%m-%d`
 rm -Rf /home/ak/nightly/geowebcache-*
 svn export http://geowebcache.org/svn/trunk/geowebcache /home/ak/nightly/geowebcache-$DATE
-zip -r geowebcache-$DATE-SRC.zip geowebcache-$DATE
+zip -r /home/ak/nightly/geowebcache-$DATE-SRC.zip geowebcache-$DATE
 cd /home/ak/nightly/geowebcache-$DATE
-echo "Enabling WAR build"
-sed -i s/'WAR -->'/'>'/g pom.xml
-sed -i s/'<!-- WAR'/'<'/g pom.xml
+#echo "Enabling WAR build"
+#sed -i s/'WAR -->'/'>'/g pom.xml
+#sed -i s/'<!-- WAR'/'<'/g pom.xml
 echo "Replacing GWC_VERSION"
-sed -i s/{GWC_VERSION}/NIGHTLY/g src/main/java/org/geowebcache/GeoWebCacheDispatcher.java
+sed -i s/{GWC_VERSION}/NIGHTLY/g core/src/main/java/org/geowebcache/GeoWebCacheDispatcher.java
 echo "Replacing GWC_BUILD_DATE"
-sed -i s/{GWC_BUILD_DATE}/${NICEDATE}/g src/main/java/org/geowebcache/GeoWebCacheDispatcher.java
+sed -i s/{GWC_BUILD_DATE}/${NICEDATE}/g core/src/main/java/org/geowebcache/GeoWebCacheDispatcher.java
 echo "Done"
 mvn clean install -Dmaven.test.skip=true
-cp LICENSE.txt target
-cd target
+cp LICENSE.txt web/target
+cd web/target
 zip /home/ak/nightly/geowebcache-$DATE-WAR.zip LICENSE.txt geowebcache.war
 cd /home/ak/nightly
 cp *.zip /var/www/html/geowebcache
--- geowebcache/tools/release.sh
+++ geowebcache/tools/release.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-exit 0
-
 VERSION=$1
 
+if [ $# -eq 0 ] 
+then
+  exit 0
+else
+
 BUILD_ROOT=/tmp/release
-XSDOC_BIN=/home/ak/xsddoc-1.0/bin/xsddoc.unix
 DATE=`date +%Y%m%d`
 NICEDATE=`date +%Y-%m-%d`
+
 rm -Rf $BUILD_ROOT
 mkdir $BUILD_ROOT
 cd $BUILD_ROOT
-#TODO Copy to tag and check out from there
-svn co http://geowebcache.org/svn/trunk
-cd trunk/geowebcache
+svn copy http://geowebcache.org/svn/trunk http://geowebcache.org/svn/tags/$VERSION -m "Tagging $VERSION"
+svn co http://geowebcache.org/svn/tags/$VERSION
+
+# For testing
+#svn co http://geowebcache.org/svn/trunk $VERSION
+cd $VERSION/geowebcache
 echo "Replacing GWC_VERSION"
 sed -i s/{GWC_VERSION}/NIGHTLY/g src/main/java/org/geowebcache/GeoWebCacheDispatcher.java
 echo "Replacing GWC_BUILD_DATE"
@@ -21,23 +27,34 @@ echo "Replacing version in POM"
 sed -i s/1\.2-SNAPSHOT/${VERSION}/g pom.xml
 echo "Done"
 
+echo "Press a key"
+read tmp
+
 # Saving a copy of the source
+echo "Packaging the source code"
 cd ..
 svn export geowebcache geowebcache-$VERSION
 zip -r ../geowebcache-$VERSION-SRC.zip geowebcache-$VERSION
+
 cd geowebcache-$VERSION
 echo "Building and deploying JAR"
-mvn clean install 
-#deploy
+mvn clean install deploy
 
 echo "Enabling WAR build"
 sed -i s/'WAR -->'/'>'/g pom.xml
 sed -i s/'<!-- WAR'/'<'/g pom.xml
 
+echo "Press a key"
+read tmp
+
 echo "Building WAR"
 mvn clean install -Dmaven.test.skip=true
 cp LICENSE.txt target
 cd target
+
+echo "Press a key"
+read tmp
+
 zip ../../../geowebcache-${VERSION}-WAR.zip LICENSE.txt geowebcache.war
 cd ../..
 cd documentation/en/user
@@ -45,13 +62,21 @@ echo "Building documentation"
 make clean html
 cd build
 mv html geowebcache-${VERSION}-docs
+
+echo "Press a key"
+read tmp
+
 echo "Packing documentation"
-zip -r ../../../../../geowebcache-${VERSION}-DOC.zip geowebcache-$VERSION-docs
+zip -r /tmp/release/geowebcache-${VERSION}-DOC.zip geowebcache-$VERSION-docs
 
 echo "Building XSD documentation"
-mkdir /tmp/release/$VERSION
 cd /tmp/release
+mkdir schema
+mkdir schema/$VERSION
 
-eval $XSDOC_BIN -o /tmp/release/$VERSION -t "GeoWebCache 1.2.1 Configuration Schema" /home/ak/gwc/trunk/src/main/resources/org/geowebcache/config/geowebcache.xsd
-cd  /tmp/release/
-zip -r $VERSION.zip $VERSION
+/home/ak/xsddoc-1.0/bin/xsddoc -o /tmp/release/schema/$VERSION -t "GeoWebCache $VERSION Configuration Schema" /tmp/release/$VERSION/geowebcache/src/main/resources/org/geowebcache/config/geowebcache.xsd
+
+cd  /tmp/release/schema
+zip -r ../$VERSION.zip $VERSION
+
+fi
