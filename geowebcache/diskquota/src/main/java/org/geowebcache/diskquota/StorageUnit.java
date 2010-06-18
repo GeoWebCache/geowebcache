e7de42f3171bbf5bae75124d23833ccf89168375
Gabriel Roldan <gabriel.roldan@gmail.com>
2010-06-18 23:35:12 +0000

    StorageUnit conversion
    git-svn-id: http://geowebcache.org/svn/trunk@1064 e7b91dd5-889a-44ae-8e97-0abfc27e49b2

--- geowebcache/diskquota/src/main/java/org/geowebcache/diskquota/StorageUnit.java
+++ geowebcache/diskquota/src/main/java/org/geowebcache/diskquota/StorageUnit.java
@@ -1,5 +1,53 @@
 package org.geowebcache.diskquota;
 
 public enum StorageUnit {
-    B, KB, MB, GB, TB
+
+    B(1) {
+        @Override
+        public double convertTo(double value, StorageUnit target) {
+            return 0;
+        }
+    },
+    KB(1024) {
+        @Override
+        public double convertTo(double value, StorageUnit target) {
+            return 0;
+        }
+    },
+    MB(1024 * 1024) {
+        @Override
+        public double convertTo(double value, StorageUnit target) {
+            return 0;
+         }
+    },
+    GB(1024 * 1024 * 1024) {
+        @Override
+        public double convertTo(double value, StorageUnit target) {
+            return 0;
+        }
+    },
+    TB(1024 * 1024 * 1024 * 1024) {
+        @Override
+        public double convertTo(double value, StorageUnit target) {
+            return 0;
+        }
+    };
+
+    protected final long bytes;
+
+    private StorageUnit(final long bytes) {
+        this.bytes = bytes;
+    }
+
+    protected final long toBytes(double value) {
+        return (long) (bytes * value);
+    }
+
+    protected final double fromBytes(long value) {
+        return value / (double) bytes;
+    }
+
+    public double convertTo(double value, StorageUnit target) {
+        return target.fromBytes(toBytes(value));
+    }
 }
