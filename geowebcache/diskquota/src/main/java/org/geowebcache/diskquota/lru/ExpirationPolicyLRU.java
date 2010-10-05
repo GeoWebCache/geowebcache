f5eff2a2767fa0141b359b24d75873a1058c74a1
groldan <groldan@e7b91dd5-889a-44ae-8e97-0abfc27e49b2>
2010-10-05 21:03:58 +0000

    missing license headers
    git-svn-id: http://geowebcache.org/svn/trunk@1197 e7b91dd5-889a-44ae-8e97-0abfc27e49b2

--- geowebcache/diskquota/src/main/java/org/geowebcache/diskquota/lru/ExpirationPolicyLRU.java
+++ geowebcache/diskquota/src/main/java/org/geowebcache/diskquota/lru/ExpirationPolicyLRU.java
@@ -1,8 +1,23 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Gabriel Roldan (OpenGeo) 2010
+ *  
+ */
 package org.geowebcache.diskquota.lru;
 
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 import org.geowebcache.diskquota.DiskQuotaMonitor;
 import org.geowebcache.diskquota.Quota;
