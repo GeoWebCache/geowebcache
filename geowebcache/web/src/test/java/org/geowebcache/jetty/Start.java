d3fda9387bd0058a8d3ff7e32c8030b6c687d893
Gabriel Roldan <gabriel.roldan@gmail.com>
2010-11-24 17:33:03 -0300

    allow graceful termination when running/debugging in eclipse by writing 'stop' at the console

--- geowebcache/web/src/test/java/org/geowebcache/jetty/Start.java
+++ geowebcache/web/src/test/java/org/geowebcache/jetty/Start.java
@@ -1,5 +1,8 @@
 package org.geowebcache.jetty;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -10,8 +13,8 @@ import org.mortbay.jetty.webapp.WebAppContext;
 
 /**
  * Jetty starter, will run GeoWebCache inside the Jetty web container.<br>
- * Useful for debugging, especially in IDE were you have direct dependencies
- * between the sources of the various modules (such as Eclipse).
+ * Useful for debugging, especially in IDE were you have direct dependencies between the sources of
+ * the various modules (such as Eclipse).
  * 
  * @author wolf
  * 
@@ -20,11 +23,9 @@ public class Start {
     private static final Log log = LogFactory.getLog(Start.class);
 
     public static void main(String[] args) {
-        Server jettyServer = null;
+        final Server jettyServer = new Server();
 
         try {
-            jettyServer = new Server();
-
             SocketConnector conn = new SocketConnector();
             String portVariable = System.getProperty("jetty.port");
             int port = parsePort(portVariable);
@@ -41,6 +42,34 @@ public class Start {
             wah.setTempDirectory(new File("target/work"));
 
             jettyServer.start();
+
+            /*
+             * Reads from System.in looking for the string "stop\n" in order to gracefully terminate
+             * the jetty server and shut down the JVM. This way we can invoke the shutdown hooks
+             * while debugging in eclipse. Can't catch CTRL-C to emulate SIGINT as the eclipse
+             * console is not propagating that event
+             */
+            Thread stopThread = new Thread() {
+                @Override
+                public void run() {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                    String line;
+                    try {
+                        while (true) {
+                            line = reader.readLine();
+                            if ("stop".equals(line)) {
+                                jettyServer.stop();
+                                System.exit(0);
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.exit(1);
+                    }
+                }
+            };
+            stopThread.setDaemon(true);
+            stopThread.run();
         } catch (Exception e) {
             log.error("Could not start the Jetty server: " + e.getMessage(), e);
 
@@ -48,8 +77,7 @@ public class Start {
                 try {
                     jettyServer.stop();
                 } catch (Exception e1) {
-                    log.error("Unable to stop the " + "Jetty server:"
-                            + e1.getMessage(), e1);
+                    log.error("Unable to stop the " + "Jetty server:" + e1.getMessage(), e1);
                 }
             }
         }
