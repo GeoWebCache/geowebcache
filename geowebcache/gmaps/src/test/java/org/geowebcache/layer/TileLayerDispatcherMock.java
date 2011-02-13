5e3d5868346577a3852064f4ccb8231ab0ef1a63
jacques <jacques@jacques-laptop>
2011-02-13 19:55:19 +0100

    Added Junit test for Gmaps parameter check

---
+++ geowebcache/gmaps/src/test/java/org/geowebcache/layer/TileLayerDispatcherMock.java
@@ -0,0 +1,20 @@
+package org.geowebcache.layer;
+
+import org.geowebcache.GeoWebCacheException;
+
+public class TileLayerDispatcherMock extends TileLayerDispatcher {
+
+	private final TileLayer layer;
+
+	public TileLayerDispatcherMock(TileLayer layer) {
+		super(null, null);
+		this.layer = layer;
+	}
+
+	@Override
+	public TileLayer getTileLayer(String layerIdent)
+			throws GeoWebCacheException {
+		return layer;
+	}
+
+}
