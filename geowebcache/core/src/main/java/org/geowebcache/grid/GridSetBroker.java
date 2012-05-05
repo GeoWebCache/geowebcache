c5285b6e5e94547ba444423dfca9da3993020ca1
Gabriel Roldan <gabriel.roldan@gmail.com>
2012-05-05 13:12:56 -0300

    relates to #103: refrain from defining WMTS and OsGeo versions of 900913 since they're too close to each other as for WMSC to distinguish which one a bounding box refers to at higher zoom levels

--- geowebcache/core/src/main/java/org/geowebcache/grid/GridSetBroker.java
+++ geowebcache/core/src/main/java/org/geowebcache/grid/GridSetBroker.java
@@ -83,19 +83,22 @@ public class GridSetBroker {
 
         gridSets.put(WORLD_EPSG3857.getName(), WORLD_EPSG3857);
 
-        log.debug("Adding WMTS 1.0 GoogleMapsCompatible Gridset");
-        GridSet googleMapsCompatible = GridSetFactory.createGridSet("GoogleMapsCompatible",
-                googleMapsCompatibleSRS, BoundingBox.WORLD3857, false,
-                googleMapsCompatibleResolutions(), null, 1.0D,
-                GridSetFactory.DEFAULT_PIXEL_SIZE_METER, null, 256, 256, false);
-
-        googleMapsCompatible
-                .setDescription("This well-known scale set has been defined to be compatible with Google Maps and"
-                        + " Microsoft Live Map projections and zoom levels. Level 0 allows representing the whole "
-                        + "world in a single 256x256 pixels. The next level represents the whole world in 2x2 tiles "
-                        + "of 256x256 pixels and so on in powers of 2. Scale denominator is only accurate near the equator.");
-
-        gridSets.put(googleMapsCompatible.getName(), googleMapsCompatible);
+        // log.debug("Adding WMTS 1.0 GoogleMapsCompatible Gridset");
+        // GridSet googleMapsCompatible = GridSetFactory.createGridSet("GoogleMapsCompatible",
+        // googleMapsCompatibleSRS, BoundingBox.WORLD3857, false,
+        // googleMapsCompatibleResolutions(), null, 1.0D,
+        // GridSetFactory.DEFAULT_PIXEL_SIZE_METER, null, 256, 256, false);
+        //
+        // googleMapsCompatible
+        // .setDescription("This well-known scale set has been defined to be compatible with Google Maps and"
+        // +
+        // " Microsoft Live Map projections and zoom levels. Level 0 allows representing the whole "
+        // +
+        // "world in a single 256x256 pixels. The next level represents the whole world in 2x2 tiles "
+        // +
+        // "of 256x256 pixels and so on in powers of 2. Scale denominator is only accurate near the equator.");
+        //
+        // gridSets.put(googleMapsCompatible.getName(), googleMapsCompatible);
 
         log.debug("Adding GlobalCRS84Pixel");
         GridSet GlobalCRS84Pixel = GridSetFactory.createGridSet("GlobalCRS84Pixel",
