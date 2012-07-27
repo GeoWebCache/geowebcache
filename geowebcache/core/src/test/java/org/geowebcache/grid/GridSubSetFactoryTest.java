08dfb9ff8cd7fe70c7491a07bf9a502873eafa6c
Gabriel Roldan <gabriel.roldan@gmail.com>
2012-07-26 23:28:56 -0300

    fixes #118. Thanks jdebarbanson

--- geowebcache/core/src/test/java/org/geowebcache/grid/GridSubSetFactoryTest.java
+++ geowebcache/core/src/test/java/org/geowebcache/grid/GridSubSetFactoryTest.java
@@ -4,32 +4,31 @@ import java.util.Arrays;
 
 import junit.framework.TestCase;
 
-
 public class GridSubSetFactoryTest extends TestCase {
-    
+
     GridSetBroker gridSetBroker = new GridSetBroker(false, false);
-    
+
     public void testCoverageBounds() throws Exception {
         BoundingBox bbox = new BoundingBox(0, 0, 180, 90);
-        
-        GridSubset grid = GridSubsetFactory.createGridSubSet(
-                gridSetBroker.WORLD_EPSG4326, bbox, 0, 0);
-        
+
+        GridSubset grid = GridSubsetFactory.createGridSubSet(gridSetBroker.WORLD_EPSG4326, bbox, 0,
+                0);
+
         long[] ret = grid.getCoverage(0);
-        long[] correct = {1, 0, 1, 0, 0};
-        
+        long[] correct = { 1, 0, 1, 0, 0 };
+
         assertTrue(Arrays.equals(correct, ret));
     }
-    
+
     public void testCoverageBounds2() throws Exception {
         BoundingBox bbox = new BoundingBox(0, 0, 180, 90);
-        
-        GridSubset grid = GridSubsetFactory.createGridSubSet(
-                gridSetBroker.WORLD_EPSG4326, bbox, 0, 1);
-        
+
+        GridSubset grid = GridSubsetFactory.createGridSubSet(gridSetBroker.WORLD_EPSG4326, bbox, 0,
+                1);
+
         long[] ret = grid.getCoverage(1);
         long[] correct = { 2, 1, 3, 1, 1 };
-        
+
         assertTrue(Arrays.equals(correct, ret));
     }
 
@@ -38,14 +37,28 @@ public class GridSubSetFactoryTest extends TestCase {
 
         int zoomStart = 3;
         int zoomStop = 9;
-        GridSubset grid = GridSubsetFactory.createGridSubSet(gridSetBroker.WORLD_EPSG4326, bbox, zoomStart,
-                zoomStop);
+        GridSubset grid = GridSubsetFactory.createGridSubSet(gridSetBroker.WORLD_EPSG4326, bbox,
+                zoomStart, zoomStop);
 
         String[] gridNames = grid.getGridNames();
         final int nlevels = 1 + (grid.getZoomStop() - grid.getZoomStart());
         assertEquals(nlevels, gridNames.length);
-        for(String name : gridNames){
+        for (String name : gridNames) {
             assertNotNull(name);
         }
+    }
+
+    public void testGridIndex() throws Exception {
+        BoundingBox bbox = new BoundingBox(0, 0, 180, 90);
+
+        int zoomStart = 3;
+        int zoomStop = 9;
+        GridSubset grid = GridSubsetFactory.createGridSubSet(gridSetBroker.WORLD_EPSG4326, bbox,
+                zoomStart, zoomStop);
+
+        String[] gridNames = grid.getGridNames();
+        for (int i = 0, z = zoomStart; i < gridNames.length; i++, z++) {
+            assertEquals(z, grid.getGridIndex(gridNames[i]));
+        }        
     }
 }
