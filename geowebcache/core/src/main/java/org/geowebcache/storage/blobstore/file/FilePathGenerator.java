51144d054b1968a5e7876973637860a7bfc50f2f
Andrea Aime <andrea.aime@gmail.com>
2012-09-21 15:31:39 +0200

    Bye bye on disk hash map

--- geowebcache/core/src/main/java/org/geowebcache/storage/blobstore/file/FilePathGenerator.java
+++ geowebcache/core/src/main/java/org/geowebcache/storage/blobstore/file/FilePathGenerator.java
@@ -37,11 +37,7 @@ public class FilePathGenerator {
     
     private static Log log = LogFactory.getLog(FilePathGenerator.class);
     
-    static final String PARAMETER_FILE = "parameters.txt";
-    Map<String, String> parameterPathCache = new ConcurrentHashMap<String, String>();
-    
     String cacheRoot;
-    
 
     public FilePathGenerator(String cacheRoot) {
         this.cacheRoot = cacheRoot;
@@ -93,7 +89,6 @@ public class FilePathGenerator {
         if (parametersId == null && parameters != null && !parameters.isEmpty()) {
             parametersId = getParametersId(tile.getLayerName(), path.toString(), parameters);
             tile.setParametersId(parametersId);
-            // path.append(Long.toHexString(parameters_id));
         }
         if(parametersId != null) {
             path.append('_');
@@ -121,121 +116,7 @@ public class FilePathGenerator {
     
     protected String getParametersId(String gridSetId, String fileBase, Map<String, String> parameters) throws IOException {
         String parametersKvp = getParametersKvp(parameters);
-        String parametersSHA = buildKey(parametersKvp);
-        String pathKey = gridSetId + parametersKvp;
-        String parametersId = parameterPathCache.get(pathKey);
-        if(log.isDebugEnabled()) {
-            log.debug("Searched in path cache for key " + pathKey + ", found " + parametersId);
-        }
-
-
-        // common case, the param ids are cached and the target dir exists already
-        if(parametersId != null) {
-            File cacheDirectory = new File(fileBase + "_" + parametersId);
-            if(cacheDirectory.exists()) {
-                if(log.isDebugEnabled()) {
-                    log.debug("Directory found at first attempt for " + pathKey + ": " + cacheDirectory.getPath());
-                } 
-                return parametersId;
-            }
-            // if it does not exist we need to synch up before creation
-        }
-        
-        synchronized (this) {
-            // check if the pathKey has been associated by another thread in the same GWC
-            // while we were waiting on the synch region. If another GWC instance created the
-            // path we won't have it in our cache
-            parametersId = parameterPathCache.get(pathKey);
-            if(parametersId != null) {
-                File cacheDirectory = new File(fileBase + "_" + parametersId);
-                if(!cacheDirectory.exists()) {
-                    if(log.isDebugEnabled()) {
-                        log.debug("Creating new cache directory for " + pathKey + ": " + cacheDirectory.getPath());
-                    }
-                    createCacheDirectory(cacheDirectory, parametersKvp);
-                } else {
-                    if(log.isDebugEnabled()) {
-                        log.debug("Directory found for " + pathKey + ": " + cacheDirectory.getPath());
-                    }
-                }
-                return parametersId;
-            }
-            
-            // ok, build the path
-            for(int i = 0; i < Integer.MAX_VALUE; i++) {
-                parametersId = parametersSHA + "_" + i;
-                File currFile = new File(fileBase + "_" + parametersId);
-                if(!currFile.exists() && !parameterCollision(parametersId)) {
-                    if(createCacheDirectory(currFile, parametersKvp)) {
-                        if(log.isDebugEnabled()) {
-                            log.debug("Created new cache directory for " + pathKey + ": " + currFile.getPath());
-                        }
-                        parameterPathCache.put(pathKey, parametersId);
-                        return parametersId;
-                    }
-                } 
-                
-                // directory exists, we need to check the parameters
-                File parameterFile = new File(currFile, PARAMETER_FILE);
-                if(!parameterFile.exists()) {
-                    // leave it alone, it may be another GWC process trying to 
-                    // write on the same directory
-                    if(log.isDebugEnabled()) {
-                        log.debug("Found empty cache directory for " + pathKey + ", skipping: " + currFile.getPath());
-                    }
-                } else {
-                    // check if the params are the same, or we're just hitting a collision
-                    String paramFileKvp = FileUtils.readFileToString(parameterFile);
-                    String currPathKey = fileBase + "_" + paramFileKvp;
-                    if(paramFileKvp.equals(parametersKvp)) {
-                        if(log.isDebugEnabled()) {
-                            log.debug("Found cache directory for " + pathKey + ", skipping: " + currFile.getPath());
-                        }
-                        parameterPathCache.put(currPathKey, paramFileKvp);
-                        return parametersId;
-                    } else {
-                        if(log.isDebugEnabled()) {
-                            log.debug("Found busy cache directory for " + pathKey + ", collision found, skipping: " + currFile.getPath());
-                        }
-                    }
-                }
-                
-            }
-        }
-        
-        throw new RuntimeException("Failed to locate an appropriate folder for the pathKey " + pathKey 
-                + ", this is likely due a programmin error");
-    }
-
-    /**
-     * Checks if a parameterId collides with something already in the cache
-     * 
-     * @param parametersId
-     * @return
-     */
-    boolean parameterCollision(String parametersId) {
-        return parameterPathCache.values().contains(parametersId);
-    }
-
-    /**
-     * Writes down the root of a cache directory and the associated parameter file
-     * 
-     * @param cacheDirectory
-     * @param parametersKvp
-     * @throws IOException
-     */
-    private boolean createCacheDirectory(File cacheDirectory, String parametersKvp) throws IOException {
-        if(!cacheDirectory.mkdirs()) {
-            if(cacheDirectory.exists()) {
-                // collision with another GWC active on the same directory, leave it alone
-                return false;
-            } else {
-                throw new IOException("Failed to create tile directory " + cacheDirectory.getAbsolutePath());
-            }
-        }
-        File parameterFile = new File(cacheDirectory, PARAMETER_FILE);
-        FileUtils.write(parameterFile, parametersKvp);
-        return true;
+        return buildKey(parametersKvp);
     }
 
     /**
--- geowebcache/core/src/test/java/org/geowebcache/blobstore/file/FilePathGeneratorTest.java
+++ geowebcache/core/src/test/java/org/geowebcache/blobstore/file/FilePathGeneratorTest.java
@@ -55,61 +55,27 @@ public class FilePathGeneratorTest extends TestCase {
 
         // first time, this will also create the path on disk
         File path = generator.tilePath(tile, ImageMime.png);
-        testParameterId(path, sha1 + "_0", "?style=population");
+        testParameterId(path, sha1, "?style=population");
         
         // another time, it should go off the pre-calculated tile id
         path = generator.tilePath(tile, ImageMime.png);
-        testParameterId(path, sha1 + "_0", "?style=population");
+        testParameterId(path, sha1, "?style=population");
         
         // this time with a separate tile, but same params
         tile = TileObject.createCompleteTileObject("states", new long[] {0, 0, 0}, "EPSG:2163", "png", params, null);
         path = generator.tilePath(tile, ImageMime.png);
-        testParameterId(path, sha1 + "_0", "?style=population");
+        testParameterId(path, sha1, "?style=population");
         
         // and now a separate tile, but different params
         params.put("style", "polygon");
         tile = TileObject.createCompleteTileObject("states", new long[] {0, 0, 0}, "EPSG:2163", "png", params, null);
         path = generator.tilePath(tile, ImageMime.png);
         sha1 = DigestUtils.shaHex("?style=polygon");
-        testParameterId(path, sha1 + "_0", "?style=polygon");
-    }
-    
-    public void testCollisionManagement() throws Exception {
-        // first time, no collisions
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("style", "population");
-        TileObject tile = TileObject.createCompleteTileObject("states", new long[] {0, 0, 0}, "EPSG:2163", "png", params, null);
-        File path = collisionGenerator.tilePath(tile, ImageMime.png);
-        testParameterId(path, "abc_0", "?style=population");
-
-        // now we'll get a collision
-        params = new HashMap<String, String>();
-        params.put("style", "polygon");
-        tile = TileObject.createCompleteTileObject("states", new long[] {0, 0, 0}, "EPSG:2163", "png", params, null);
-        path = collisionGenerator.tilePath(tile, ImageMime.png);
-        testParameterId(path, "abc_1", "?style=polygon");
-        
-        // remove the first zoom directory, assuming it was emptied somehow
-        File paramCacheRoot = new File(testRoot, "states/EPSG_2163_00/abc_0");
-        FileUtils.deleteDirectory(paramCacheRoot);
-        
-        // generate another one, it should not end up reusing the abc_0 which was removed by the user
-        // (otherwise we'll end up with two sets of params having the same id in the file generator cache)
-        params = new HashMap<String, String>();
-        params.put("style", "hatches");
-        tile = TileObject.createCompleteTileObject("states", new long[] {0, 0, 0}, "EPSG:2163", "png", params, null);
-        path = collisionGenerator.tilePath(tile, ImageMime.png);
-        testParameterId(path, "abc_2", "?style=hatches");
-        
+        testParameterId(path, sha1, "?style=polygon");
     }
 
     private void testParameterId(File path, String parameterId, String parameterKvp) throws IOException {
         File expected = new File(testRoot, "states/EPSG_2163_00_" + parameterId + "/0_0/00_00.png");
         assertEquals(expected.getPath(), path.getPath());
-        File paramCacheRoot = new File(testRoot, "states/EPSG_2163_00_" + parameterId);
-        assertTrue(paramCacheRoot.exists());
-        File paramFile = new File(testRoot, "states/EPSG_2163_00_" + parameterId + "/parameters.txt");
-        assertTrue(paramFile.exists());
-        assertEquals(parameterKvp, FileUtils.readFileToString(paramFile));
     }
 }
