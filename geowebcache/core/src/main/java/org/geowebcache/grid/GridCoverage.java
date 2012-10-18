64d60ccf8738fe30e525f78715324914bb0ea0a1
Gabriel Roldan <gabriel.roldan@gmail.com>
2012-10-18 18:42:18 -0300

    backport of pull request #140, fixes #61.

--- geowebcache/core/src/main/java/org/geowebcache/grid/GridCoverage.java
+++ geowebcache/core/src/main/java/org/geowebcache/grid/GridCoverage.java
@@ -10,10 +10,17 @@ public class GridCoverage {
     protected long[] coverage;
 
     protected GridCoverage(long[] coverage) {
+        // TODO: should check that the coverage has exactly 5 elements.
         this.coverage = coverage;
     }
 
+    /**
+     * Find the intersection of the given rectangle with the coverage
+     * @param rectangle Array of long, minx,miny,maxx,maxy,level, in tile coordinates.
+     * @return Array of long representing the intersection, minx,miny,maxx,maxy,level, in tile coordinates 
+     */
     protected long[] getIntersection(long[] rectangle) {
+        // TODO: should check that the rectangle has 5 elements and that the fifth (level) matches that of the coverage
         long[] ret = { Math.min(Math.max(coverage[0], rectangle[0]), coverage[2]),
                 Math.min(Math.max(coverage[1], rectangle[1]), coverage[3]),
                 Math.min(Math.max(coverage[0], rectangle[2]), coverage[2]),
