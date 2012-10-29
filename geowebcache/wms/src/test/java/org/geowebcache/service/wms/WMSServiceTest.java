1c78943fc2aaaa6678b00a25d80022f695135c45
Kevin Smith <ksmith@opengeo.org>
2012-10-29 14:58:31 -0700

    Detect if GWC is embeded within a parent servlet and include the context path of the parent in the base URL for mangling purposes.

--- geowebcache/wms/src/test/java/org/geowebcache/service/wms/WMSServiceTest.java
+++ geowebcache/wms/src/test/java/org/geowebcache/service/wms/WMSServiceTest.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import junit.framework.TestCase;
 
 import org.apache.commons.collections.map.CaseInsensitiveMap;
+import org.geowebcache.GeoWebCacheDispatcher;
 import org.geowebcache.config.XMLGridSubset;
 import org.geowebcache.conveyor.ConveyorTile;
 import org.geowebcache.grid.BoundingBox;
@@ -70,7 +71,10 @@ public class WMSServiceTest extends TestCase {
     private void testMultipleCrsMatchingGridSubsets(final String srs, final String expectedGridset,
             long[] tileIndex) throws Exception {
 
-        service = new WMSService(sb, tld, mock(RuntimeStats.class), new NullURLMangler());
+        GeoWebCacheDispatcher gwcd = mock(GeoWebCacheDispatcher.class);
+        when(gwcd.getServletPrefix()).thenReturn(null);
+        
+        service = new WMSService(sb, tld, mock(RuntimeStats.class), new NullURLMangler(), gwcd);
 
         @SuppressWarnings("unchecked")
         Map<String, String> kvp = new CaseInsensitiveMap();
