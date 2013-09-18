557e151553358231764f9708dd2a1b27c119831d
jdeolive <jdeolive@opengeo.org>
2013-09-18 13:54:42 -0600

    Revert "Setting version to 1.5.0"
    This reverts commit 1a5a36b86467c39d08d0d5f5a95b1dbe2cd7c62a.

--- documentation/en/user/source/conf.py
+++ documentation/en/user/source/conf.py
@@ -47,7 +47,7 @@ copyright = u'OpenGeo, License: Creative Commons 3.0 - Attribution Share Alike'
 # The short X.Y version.
 version = '1.5'
 # The full version, including alpha/beta/rc tags.
-release = '1.5.0'
+release = '1.5.x'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
