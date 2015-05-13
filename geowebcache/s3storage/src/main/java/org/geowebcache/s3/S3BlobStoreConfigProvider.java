7e884dd299f2bd4f16f7c76f9e1686855ab13f6c
Gabriel Roldan <groldan@boundlessgeo.com>
2015-05-13 15:52:05 -0300

    interpret empty elements as null

--- geowebcache/s3storage/src/main/java/org/geowebcache/s3/S3BlobStoreConfigProvider.java
+++ geowebcache/s3storage/src/main/java/org/geowebcache/s3/S3BlobStoreConfigProvider.java
@@ -18,13 +18,43 @@ package org.geowebcache.s3;
 
 import org.geowebcache.config.XMLConfigurationProvider;
 
+import com.google.common.base.Strings;
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.SingleValueConverter;
+import com.thoughtworks.xstream.converters.basic.BooleanConverter;
+import com.thoughtworks.xstream.converters.basic.IntConverter;
 
 public class S3BlobStoreConfigProvider implements XMLConfigurationProvider {
+
+    private static SingleValueConverter NullableIntConverter = new IntConverter() {
+
+        @Override
+        public Object fromString(String str) {
+            if (Strings.isNullOrEmpty(str)) {
+                return null;
+            }
+            return super.fromString(str);
+        }
+    };
+
+    private static SingleValueConverter NullableBooleanConverter = new BooleanConverter() {
+
+        @Override
+        public Object fromString(String str) {
+            if (Strings.isNullOrEmpty(str)) {
+                return null;
+            }
+            return super.fromString(str);
+        }
+    };
 
     @Override
     public XStream getConfiguredXStream(XStream xs) {
         xs.alias("S3BlobStore", S3BlobStoreConfig.class);
+        xs.registerLocalConverter(S3BlobStoreConfig.class, "maxConnections", NullableIntConverter);
+        xs.registerLocalConverter(S3BlobStoreConfig.class, "proxyPort", NullableIntConverter);
+        xs.registerLocalConverter(S3BlobStoreConfig.class, "useHTTPS", NullableBooleanConverter);
+        xs.registerLocalConverter(S3BlobStoreConfig.class, "useGzip", NullableBooleanConverter);
         return xs;
     }
 
