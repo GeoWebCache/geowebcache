9c29d0eb6dced2a4057c7b68fd88106b58ab9f38
Kevin Smith <ksmith@boundlessgeo.com>
2016-04-14 15:41:29 -0700

    Ensure ByteStreamerRestlet can't be used to retrieve unintended resources.

--- geowebcache/rest/src/main/java/org/geowebcache/rest/webresources/ByteStreamerRestlet.java
+++ geowebcache/rest/src/main/java/org/geowebcache/rest/webresources/ByteStreamerRestlet.java
@@ -3,6 +3,8 @@ package org.geowebcache.rest.webresources;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
+import java.util.regex.Pattern;
 
 import org.geowebcache.mime.MimeException;
 import org.geowebcache.mime.MimeType;
@@ -17,6 +19,8 @@ import org.restlet.resource.OutputRepresentation;
 
 public class ByteStreamerRestlet extends GWCRestlet {
     
+    Class<?> context = ByteStreamerRestlet.class;
+    
     public void handle(Request request, Response response) {
         Method met = request.getMethod();
         if (met.equals(Method.GET)) {
@@ -25,13 +29,22 @@ public class ByteStreamerRestlet extends GWCRestlet {
             throw new RestletException("Method not allowed", Status.CLIENT_ERROR_METHOD_NOT_ALLOWED);
         }
     }
-
+    
+    protected URL getResource(String path) {
+        return context.getResource(path);
+    }
+    
+    static final Pattern UNSAFE_RESOURCE = Pattern.compile("^/|/\\.\\./|^\\.\\./|\\.class$"); 
     private void doGet(Request request, Response response) {
         String filename = (String) request.getAttributes().get("filename");
         
-        InputStream is = ByteStreamerRestlet.class.getResourceAsStream(filename);
+        // Just to make sure we don't allow access to arbitrary resources
+        if(UNSAFE_RESOURCE.matcher(filename).find()) {
+            throw new RestletException("Illegal web resource", Status.CLIENT_ERROR_FORBIDDEN);
+        }
         
-        if(is == null) {
+        URL resource = getResource(filename);
+        if(resource == null) {
             response.setStatus(Status.CLIENT_ERROR_NOT_FOUND);
             return;
         }
@@ -50,30 +63,31 @@ public class ByteStreamerRestlet extends GWCRestlet {
             return;
         }
         
-        ByteRepresentation imgRep = new ByteRepresentation(new MediaType(mime.getMimeType()), is);
+        ByteRepresentation imgRep = new ByteRepresentation(new MediaType(mime.getMimeType()), resource);
         
         response.setEntity(imgRep);
     }
-
+    
     private class ByteRepresentation extends OutputRepresentation {
-        InputStream is = null;
-
-        public ByteRepresentation(MediaType mediaType, InputStream is) {
+        URL resourceURL;
+        
+        public ByteRepresentation(MediaType mediaType, URL resourceURL) {
             super(mediaType);
-            this.is = is;
+            this.resourceURL = resourceURL;
         }
-
+        
         public void write(OutputStream os) throws IOException {
-            int count = 0;
-            byte[] tmp = new byte[2048];
-            
-            while(count != -1) {
-                count = is.read(tmp);
-                if(count != -1) {
-                    os.write(tmp, 0, count);
+            try( InputStream is = resourceURL.openStream(); ){
+                int count = 0;
+                byte[] tmp = new byte[2048];
+                
+                while(count != -1) {
+                    count = is.read(tmp);
+                    if(count != -1) {
+                        os.write(tmp, 0, count);
+                    }
                 }
             }
-                
         }  
     }
 }
