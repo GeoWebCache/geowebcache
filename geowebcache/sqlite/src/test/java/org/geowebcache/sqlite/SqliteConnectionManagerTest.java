678f6f544d0093865fbbbe96689e84da21a54208
Nuno Oliveira <nuno.oliveira@geo-solutions.it>
2016-06-05 15:11:05 +0100

    Add mbtiles blobstore

---
+++ geowebcache/sqlite/src/test/java/org/geowebcache/sqlite/SqliteConnectionManagerTest.java
@@ -0,0 +1,197 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Nuno Oliveira, GeoSolutions S.A.S., Copyright 2016
+ */
+package org.geowebcache.sqlite;
+
+import org.geowebcache.storage.StorageException;
+import org.junit.Test;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import static org.geowebcache.sqlite.Utils.Tuple;
+import static org.geowebcache.sqlite.Utils.Tuple.tuple;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public final class SqliteConnectionManagerTest extends TestSupport {
+
+    @Test
+    public void testGetConnection() throws StorageException {
+        SqliteConnectionManager connectionManager = new SqliteConnectionManager(Integer.MAX_VALUE, 1000);
+        connectionManager.doWork(buildRootFile("tiles", "data_base.sqlite"), false, connection -> {
+            insertInTestTable(connection, "name", "europe");
+        });
+        connectionManager.reapAllConnections();
+        assertThat(connectionManager.getPool().size(), is(0));
+        connectionManager.doWork(buildRootFile("tiles", "data_base.sqlite"), true, connection -> {
+            String value = getFromTestTable(connection, "name");
+            assertThat(value, notNullValue());
+            assertThat(value, is("europe"));
+            closeConnectionQuietly(connection);
+        });
+    }
+
+    @Test
+    public void testMultiThreadsWithSingleFile() throws Exception {
+        genericMultiThreadsTest(10, 500, Integer.MAX_VALUE, buildRootFile("data_base_a.sqlite"));
+    }
+
+    @Test
+    public void testMultiThreadsWithMultipleFiles() throws Exception {
+        genericMultiThreadsTest(10, 500, 10,
+                buildRootFile("data_base_a.sqlite"),
+                buildRootFile("data_base_b.sqlite"),
+                buildRootFile("data_base_c.sqlite"),
+                buildRootFile("data_base_d.sqlite"),
+                buildRootFile("data_base_e.sqlite")
+        );
+    }
+
+    @Test
+    public void testMultiThreadsWithMultipleFilesWithCacheLimit() throws Exception {
+        genericMultiThreadsTest(10, 500, 1,
+                buildRootFile("data_base_a.sqlite"),
+                buildRootFile("data_base_b.sqlite"),
+                buildRootFile("data_base_c.sqlite"),
+                buildRootFile("data_base_d.sqlite"),
+                buildRootFile("data_base_e.sqlite")
+        );
+    }
+
+    @Test
+    public void testReplaceOperation() throws Exception {
+        SqliteConnectionManager connectionManager = new SqliteConnectionManager(Integer.MAX_VALUE, 1000);
+        File file1 = buildRootFile("tiles", "data_base_1.sqlite");
+        Utils.createFileParents(file1);
+        File file2 = buildRootFile("tiles", "data_base_2.sqlite");
+        Utils.createFileParents(file2);
+        connectionManager.doWork(file1, false, connection -> {
+            insertInTestTable(connection, "name", "europe");
+            closeConnectionQuietly(connection);
+        });
+        file2.createNewFile();
+        connectionManager.replace(file1, file2);
+        connectionManager.doWork(file1, false, connection -> {
+            createTestTable(connection);
+            String value = getFromTestTable(connection, "name");
+            assertThat(value, nullValue());
+            closeConnectionQuietly(connection);
+        });
+    }
+
+    private static void genericMultiThreadsTest(int threadsNumber, int workersNumber,
+                                                long poolSize, File... files) throws Exception {
+        SqliteConnectionManager connectionManager = new SqliteConnectionManager(poolSize, 10);
+        ExecutorService executor = Executors.newFixedThreadPool(threadsNumber);
+        Random random = new Random();
+        List<Callable<Tuple<File, String>>> workers = new ArrayList<>();
+        for (int i = 0; i < workersNumber; i++) {
+            workers.add(() -> {
+                File file = files[random.nextInt(files.length)];
+                String key = UUID.randomUUID().toString();
+                return connectionManager.doWork(file, false, connection -> {
+                    insertInTestTable(connection, key, "value-" + key);
+                    closeConnectionQuietly(connection);
+                    return tuple(file, key);
+                });
+            });
+        }
+        List<Future<Tuple<File, String>>> results = executor.invokeAll(workers);
+        executor.shutdown();
+        executor.awaitTermination(30, TimeUnit.SECONDS);
+        connectionManager.reapAllConnections();
+        assertThat(connectionManager.getPool().size(), is(0));
+        for (Future<Tuple<File, String>> result : results) {
+            File file = result.get().first;
+            String key = result.get().second;
+            connectionManager.doWork(file, true, connection -> {
+                String value = getFromTestTable(connection, key);
+                assertThat(value, notNullValue());
+                assertThat(value, is("value-" + key));
+                closeConnectionQuietly(connection);
+            });
+        }
+    }
+
+    private static void closeConnectionQuietly(Connection connection) {
+        try {
+            connection.close();
+        } catch (Exception exception) {
+            throw Utils.exception(exception, "Error closing connection.");
+        }
+    }
+
+    private static void createTestTable(Connection connection) {
+        execute(connection, "CREATE TABLE IF NOT EXISTS test " +
+                "(key text, value text, CONSTRAINT pk_metadata PRIMARY KEY(key));");
+    }
+
+    private static void insertInTestTable(Connection connection, String key, String value) {
+        createTestTable(connection);
+        execute(connection, "INSERT INTO test VALUES ('%s', '%s');", key, value);
+    }
+
+    private static String getFromTestTable(Connection connection, String key) {
+        return new ExecuteQuery(connection, "SELECT value FROM test WHERE key = '%s' ORDER BY key;", key) {
+
+            String result;
+
+            @Override
+            public void extract(ResultSet resultSet) throws Exception {
+                if (resultSet.next()) {
+                    result = resultSet.getString(1);
+                }
+            }
+        }.result;
+    }
+
+    private static void execute(Connection connection, String sql, Object... arguments) {
+        String finalSql = String.format(sql, arguments);
+        try (PreparedStatement statement = connection.prepareStatement(finalSql)) {
+            statement.execute();
+        } catch (Exception exception) {
+            throw Utils.exception(exception, "Error executing SQL '%s'.", finalSql);
+        }
+    }
+
+    private static abstract class ExecuteQuery {
+
+        public abstract void extract(ResultSet resultSet) throws Exception;
+
+        public ExecuteQuery(Connection connection, String query, Object... arguments) {
+            String finalQuery = String.format(query, arguments);
+            try (PreparedStatement statement = connection.prepareStatement(finalQuery)) {
+                extract(statement.executeQuery());
+            } catch (Exception exception) {
+                throw Utils.exception(exception, "Error executing query '%s'.", finalQuery);
+            }
+        }
+    }
+}
---
+++ geowebcache/sqlite/src/test/java/org/geowebcache/sqlite/TestSupport.java
@@ -0,0 +1,108 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Nuno Oliveira, GeoSolutions S.A.S., Copyright 2016
+ */
+package org.geowebcache.sqlite;
+
+import org.apache.commons.io.FileUtils;
+import org.geowebcache.io.ByteArrayResource;
+import org.geowebcache.io.Resource;
+import org.junit.After;
+import org.junit.Before;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+
+/**
+ * Helper class with some utilities methods for using during the tests.
+ */
+abstract class TestSupport {
+
+    private File rootDirectory;
+
+    @Before
+    public void beforeTest() throws Exception {
+        rootDirectory = Files.createTempDirectory("gwc-").toFile();
+    }
+
+    @After
+    public void afterTest() throws Exception {
+        FileUtils.deleteQuietly(rootDirectory);
+    }
+
+    protected File getRootDirectory() {
+        return rootDirectory;
+    }
+
+    protected String getRootDirectoryPath() {
+        try {
+            return rootDirectory.getCanonicalPath();
+        } catch (Exception exception) {
+            throw Utils.exception(exception, "Error getting root directory canonical path.");
+        }
+    }
+
+    protected File createFileInRootDir(String path) throws Exception {
+        File file = new File(rootDirectory, path);
+        if (file.exists()) {
+            return null;
+        }
+        File parent = file.getParentFile();
+        if (parent != null) {
+            parent.mkdirs();
+        }
+        file.createNewFile();
+        return file;
+    }
+
+    protected File buildRootFile(String... pathParts) {
+        return new File(rootDirectory, Utils.buildPath(pathParts));
+    }
+
+    protected File buildFile(String... pathParts) {
+        return new File(Utils.buildPath(pathParts));
+    }
+
+    protected void writeToFile(File file, String content) {
+        Utils.createFileParents(file);
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(content);
+            writer.flush();
+            writer.close();
+        } catch (Exception exception) {
+            throw Utils.exception(exception, "Error creating or writing content to file '%s'.", file);
+        }
+    }
+
+    protected MbtilesConfiguration getDefaultConfiguration() {
+        MbtilesConfiguration configuration = new MbtilesConfiguration();
+        configuration.setPoolSize(1000);
+        configuration.setRootDirectory(getRootDirectory().getPath());
+        configuration.setTemplatePath(Utils.buildPath("{grid}", "{layer}", "{format}", "{z}", "tiles-{x}-{y}.sqlite"));
+        configuration.setRowRangeCount(500);
+        configuration.setColumnRangeCount(500);
+        return configuration;
+    }
+
+    protected static Resource stringToResource(String string) {
+        return new ByteArrayResource(string.getBytes());
+    }
+
+    protected static String resourceToString(Resource resource) {
+        return new String(Utils.resourceToByteArray(resource));
+    }
+}
