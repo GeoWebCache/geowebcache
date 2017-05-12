5206c560616dbb0fc9ff59cfc19576f7110a6b5b
Stuart Adam <sadam@os.uk>
2017-05-12 09:15:51 +0100

    Squashed commit of the following:
        A blob store to allow a layered n-tier blob storage hierarchy.
        Allow id references in geowebcache.xml
    
        This allows the use of nestedblobstore configuration as follows
    
        <NestedBlobStore>
        <id>files3</id>
        <frontStoreConfig reference="file" />
        <backingStoreConfig reference="s3" />
        <enabled>true</enabled>
        </NestedBlobStore>
        </blobStores>

---
+++ geowebcache/nestedstorage/src/main/java/org/geowebcache/nested/NestedBlobStore.java
@@ -0,0 +1,157 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Stuart Adam, Ordnance Survey, Copyright 2017
+ */
+package org.geowebcache.nested;
+
+import org.geowebcache.storage.BlobStore;
+import org.geowebcache.storage.BlobStoreListener;
+import org.geowebcache.storage.StorageException;
+import org.geowebcache.storage.TileObject;
+import org.geowebcache.storage.TileRange;
+
+public class NestedBlobStore implements BlobStore {
+
+    private BlobStore frontStore;
+
+    private BlobStore backingStore;
+
+    public NestedBlobStore(NestedBlobStoreConfig config) {
+        setFrontStore(config.getFrontStore());
+        setBackingStore(config.getBackingStore());
+    }
+
+    public void setFrontStore(BlobStore frontStore) {
+        this.frontStore = frontStore;
+    }
+
+    public void setBackingStore(BlobStore backingStore) {
+        this.backingStore = backingStore;
+    }
+
+    @Override
+    public boolean deleteByGridsetId(String layerName, String gridSetId) throws StorageException {
+        boolean ret = false;
+        try {
+            ret = backingStore.deleteByGridsetId(layerName, gridSetId);
+        } finally {
+            frontStore.deleteByGridsetId(layerName, gridSetId);
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean delete(String layerName) throws StorageException {
+        boolean ret = false;
+        try {
+            ret = backingStore.delete(layerName);
+        } finally {
+            frontStore.delete(layerName);
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean delete(TileObject obj) throws StorageException {
+        boolean ret = false;
+        try {
+            ret = backingStore.delete(obj);
+        } finally {
+            frontStore.delete(obj);
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean delete(TileRange obj) throws StorageException {
+        boolean ret = false;
+        try {
+            ret = backingStore.delete(obj);
+        } finally {
+            frontStore.delete(obj);
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean get(TileObject obj) throws StorageException {
+        boolean answer = frontStore.get(obj);
+        if (!answer) {
+            answer = backingStore.get(obj);
+            if (answer) {
+                frontStore.put(obj);
+            }
+        }
+        return answer;
+    }
+
+    @Override
+    public void put(TileObject obj) throws StorageException {
+        backingStore.put(obj);
+        frontStore.put(obj);
+    }
+
+    @Override
+    public void clear() throws StorageException {
+        throw new UnsupportedOperationException("clear() should not be called");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void addListener(BlobStoreListener listener) {
+        backingStore.addListener(listener);
+    }
+
+    @Override
+    public boolean removeListener(BlobStoreListener listener) {
+        return backingStore.removeListener(listener);
+    }
+
+    @Override
+    public boolean rename(String oldLayerName, String newLayerName) throws StorageException {
+        boolean answer = false;
+        try {
+            answer = backingStore.rename(oldLayerName, newLayerName);
+        } finally {
+            frontStore.rename(oldLayerName, newLayerName);
+        }
+        return answer;
+    }
+
+    @Override
+    public String getLayerMetadata(String layerName, String key) {
+        String layerMetaData = frontStore.getLayerMetadata(layerName, key);
+        if (null == layerMetaData) {
+            layerMetaData = backingStore.getLayerMetadata(layerName, key);
+            frontStore.putLayerMetadata(layerName, key, layerMetaData);
+        }
+        return layerMetaData;
+    }
+
+    @Override
+    public void putLayerMetadata(String layerName, String key, String value) {
+        backingStore.putLayerMetadata(layerName, key, value);
+        frontStore.putLayerMetadata(layerName, key, value);
+    }
+
+    @Override
+    public boolean layerExists(String layerName) {
+        return frontStore.layerExists(layerName) || backingStore.layerExists(layerName);
+    }
+}
---
+++ geowebcache/nestedstorage/src/test/java/org/geowebcache/nested/NestedBlobStoreTest.java
@@ -0,0 +1,369 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Stuart Adam, Ordnance Survey, Copyright 2017
+ */
+package org.geowebcache.nested;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.geowebcache.mime.MimeException;
+import org.geowebcache.mime.MimeType;
+import org.geowebcache.storage.BlobStore;
+import org.geowebcache.storage.BlobStoreListener;
+import org.geowebcache.storage.StorageException;
+import org.geowebcache.storage.TileObject;
+import org.geowebcache.storage.TileRange;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.google.common.base.Throwables;
+
+public class NestedBlobStoreTest {
+    private static final String DEFAULT_FORMAT = "png";
+
+    private static final String DEFAULT_GRIDSET = "EPSG:4326";
+
+    private static final String DEFAULT_LAYER = "topp:world";
+
+    @Mock
+    BlobStore frontStore;
+
+    @Mock
+    BlobStore backingStore;
+
+    @Mock
+    NestedBlobStoreConfig config;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetFromFrontStore() throws StorageException {
+        TileObject to = configureQuery();
+        NestedBlobStore store = configureStore();
+
+        Mockito.when(frontStore.get(to)).thenReturn(true);
+
+        store.get(to);
+        Mockito.verify(frontStore).get(to);
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testGetFromBackingStoreAndPopulateFrontStore() throws StorageException {
+        TileObject to = configureQuery();
+        NestedBlobStore store = configureStore();
+
+        Mockito.when(frontStore.get(to)).thenReturn(false);
+        Mockito.when(backingStore.get(to)).thenReturn(true);
+
+        store.get(to);
+        Mockito.verify(frontStore).get(to);
+        Mockito.verify(backingStore).get(to);
+        Mockito.verify(frontStore).put(to);
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testFailedGetFromFrontBackingStore() throws StorageException {
+        TileObject to = configureQuery();
+        NestedBlobStore store = configureStore();
+
+        Mockito.when(frontStore.get(to)).thenReturn(false);
+        Mockito.when(backingStore.get(to)).thenReturn(false);
+
+        store.get(to);
+        Mockito.verify(frontStore).get(to);
+        Mockito.verify(backingStore).get(to);
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testPut() throws StorageException {
+        TileObject to = configureQuery();
+        NestedBlobStore store = configureStore();
+
+        store.put(to);
+        Mockito.verify(frontStore).put(to);
+        Mockito.verify(backingStore).put(to);
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testGetLayerMetaDataFrontStore() throws StorageException {
+        TileObject to = configureQuery();
+        NestedBlobStore store = configureStore();
+
+        String key = "key";
+        String layerName = "layer";
+        Mockito.when(frontStore.getLayerMetadata(layerName, key)).thenReturn("value");
+
+        store.getLayerMetadata(layerName, key);
+        Mockito.verify(frontStore).getLayerMetadata(layerName, key);
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testGetLayerMetaDataBackingStoreIfFrontStoreEmpty() throws StorageException {
+        NestedBlobStore store = configureStore();
+
+        String key = "key";
+        String layerName = "layer";
+        Mockito.when(frontStore.getLayerMetadata(layerName, key)).thenReturn(null);
+        Mockito.when(backingStore.getLayerMetadata(layerName, key)).thenReturn("value");
+
+        store.getLayerMetadata(layerName, key);
+        Mockito.verify(frontStore).getLayerMetadata(layerName, key);
+        Mockito.verify(backingStore).getLayerMetadata(layerName, key);
+        Mockito.verify(frontStore).putLayerMetadata(layerName, key, "value");
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testPutLayerMetaData() throws StorageException {
+        NestedBlobStore store = configureStore();
+
+        String key = "key";
+        String layerName = "layer";
+        String value = "value";
+
+        store.putLayerMetadata(layerName, key, value);
+        Mockito.verify(frontStore).putLayerMetadata(layerName, key, value);
+        Mockito.verify(backingStore).putLayerMetadata(layerName, key, value);
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testDeleteTileObject() throws StorageException {
+        TileObject to = configureQuery();
+        NestedBlobStore store = configureStore();
+
+        store.delete(to);
+        Mockito.verify(frontStore).delete(to);
+        Mockito.verify(backingStore).delete(to);
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testDeleteTileRange() throws StorageException, MimeException {
+        long[][] rangeBounds = { //
+                { 0, 0, 0, 0, 0 }, //
+                { 0, 0, 1, 1, 1 }, //
+                { 0, 0, 3, 3, 2 } //
+        };
+
+        MimeType mimeType = MimeType.createFromExtension(DEFAULT_FORMAT);
+
+        Map<String, String> parameters = null;
+
+        final int truncateStart = 0, truncateStop = 1;
+
+        TileRange tileRange = tileRange(DEFAULT_LAYER, DEFAULT_GRIDSET, truncateStart, truncateStop,
+                rangeBounds, mimeType, parameters);
+        NestedBlobStore store = configureStore();
+
+        store.delete(tileRange);
+        Mockito.verify(frontStore).delete(tileRange);
+        Mockito.verify(backingStore).delete(tileRange);
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testDeleteLayerName() throws StorageException {
+        String layer = "layer";
+        NestedBlobStore store = configureStore();
+
+        store.delete(layer);
+        Mockito.verify(frontStore).delete(layer);
+        Mockito.verify(backingStore).delete(layer);
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testDeleteByGridsetId() throws StorageException {
+        String layer = "layer";
+        String gridSet = "gridSet";
+        NestedBlobStore store = configureStore();
+
+        store.deleteByGridsetId(layer, gridSet);
+        Mockito.verify(frontStore).deleteByGridsetId(layer, gridSet);
+        Mockito.verify(backingStore).deleteByGridsetId(layer, gridSet);
+        Mockito.verifyNoMoreInteractions(frontStore, backingStore);
+    }
+
+    @Test
+    public void testRename() throws StorageException {
+        NestedBlobStore store = configureStore();
+        String oldLayerName = "oldLayerName";
+        String newLayerName = "newLayerName";
+        store.rename(oldLayerName, newLayerName);
+        Mockito.verify(frontStore).rename(oldLayerName, newLayerName);
+        Mockito.verify(backingStore).rename(oldLayerName, newLayerName);
+        Mockito.verifyZeroInteractions(frontStore, backingStore);
+    }
+    
+    @Test
+    public void testLayerExists() {
+        NestedBlobStore store = configureStore();
+        Mockito.when(frontStore.layerExists("frontLayerName")).thenReturn(true);
+        Mockito.when(frontStore.layerExists("backLayerName")).thenReturn(false);
+        Mockito.when(backingStore.layerExists("backLayerName")).thenReturn(true);
+        assertTrue(store.layerExists("frontLayerName"));
+        assertTrue(store.layerExists("backLayerName"));
+        assertFalse(store.layerExists("nonExistantLayerName"));
+    }
+
+    @Test
+    public void testAddBlobStoreListener() {
+        NestedBlobStore store = configureStore();
+        BlobStoreListener listener = new BlobStoreListener() {
+            @Override
+            public void tileStored(String layerName, String gridSetId, String blobFormat,
+                    String parametersId, long x, long y, int z, long blobSize) {
+
+            }
+
+            @Override
+            public void tileDeleted(String layerName, String gridSetId, String blobFormat,
+                    String parametersId, long x, long y, int z, long blobSize) {
+
+            }
+
+            @Override
+            public void tileUpdated(String layerName, String gridSetId, String blobFormat,
+                    String parametersId, long x, long y, int z, long blobSize, long oldSize) {
+
+            }
+
+            @Override
+            public void layerDeleted(String layerName) {
+
+            }
+
+            @Override
+            public void layerRenamed(String oldLayerName, String newLayerName) {
+
+            }
+
+            @Override
+            public void gridSubsetDeleted(String layerName, String gridSetId) {
+
+            }
+        };
+        store.addListener(listener);
+        Mockito.verify(backingStore).addListener(listener);
+        Mockito.verifyZeroInteractions(frontStore);
+        Mockito.verifyNoMoreInteractions(backingStore);
+    }
+
+    @Test
+    public void testRemoveBlobStoreListener() {
+        NestedBlobStore store = configureStore();
+        BlobStoreListener listener = new BlobStoreListener() {
+            @Override
+            public void tileStored(String layerName, String gridSetId, String blobFormat,
+                    String parametersId, long x, long y, int z, long blobSize) {
+
+            }
+
+            @Override
+            public void tileDeleted(String layerName, String gridSetId, String blobFormat,
+                    String parametersId, long x, long y, int z, long blobSize) {
+
+            }
+
+            @Override
+            public void tileUpdated(String layerName, String gridSetId, String blobFormat,
+                    String parametersId, long x, long y, int z, long blobSize, long oldSize) {
+
+            }
+
+            @Override
+            public void layerDeleted(String layerName) {
+
+            }
+
+            @Override
+            public void layerRenamed(String oldLayerName, String newLayerName) {
+
+            }
+
+            @Override
+            public void gridSubsetDeleted(String layerName, String gridSetId) {
+
+            }
+        };
+        store.removeListener(listener);
+        Mockito.verify(backingStore).removeListener(listener);
+        Mockito.verifyZeroInteractions(frontStore);
+        Mockito.verifyNoMoreInteractions(backingStore);
+    }
+
+    private NestedBlobStore configureStore() {
+        config = Mockito.mock(NestedBlobStoreConfig.class);
+        Mockito.when(config.getBackingStore()).thenReturn(backingStore);
+        Mockito.when(config.getFrontStore()).thenReturn(frontStore);
+        NestedBlobStore store;
+        store = new NestedBlobStore(config);
+        return store;
+    }
+
+    private TileObject configureQuery() {
+        TileObject to;
+        to = queryTile(1, 2, 3);
+        return to;
+    }
+
+    private TileObject queryTile(long x, long y, int z) {
+        return queryTile(DEFAULT_LAYER, DEFAULT_GRIDSET, DEFAULT_FORMAT, x, y, z);
+    }
+
+    private TileObject queryTile(String layer, String gridset, String extension, long x, long y,
+            int z) {
+        return queryTile(layer, gridset, extension, x, y, z, (Map<String, String>) null);
+    }
+
+    private TileObject queryTile(String layer, String gridset, String extension, long x, long y,
+            int z, Map<String, String> parameters) {
+
+        String format;
+        try {
+            format = MimeType.createFromExtension(extension).getFormat();
+        } catch (MimeException e) {
+            throw Throwables.propagate(e);
+        }
+
+        TileObject tile = TileObject.createQueryTileObject(layer, new long[] { x, y, z }, gridset,
+                format, parameters);
+        return tile;
+    }
+
+    private TileRange tileRange(String layerName, String gridSetId, int zoomStart, int zoomStop,
+            long[][] rangeBounds, MimeType mimeType, Map<String, String> parameters) {
+
+        TileRange tileRange = new TileRange(layerName, gridSetId, zoomStart, zoomStop, rangeBounds,
+                mimeType, parameters);
+        return tileRange;
+    }
+}
