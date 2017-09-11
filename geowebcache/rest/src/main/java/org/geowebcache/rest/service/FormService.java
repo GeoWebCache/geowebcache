4a0881483e74a1540de191f2a3666e37cb4584a1
vickdw <dvick@boundlessgeo.com>
2017-09-11 12:31:20 -0500

    adding headers and fixing endpoint for the rest home page.

--- geowebcache/rest/src/main/java/org/geowebcache/rest/service/FormService.java
+++ geowebcache/rest/src/main/java/org/geowebcache/rest/service/FormService.java
@@ -1,3 +1,25 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Marius Suta / The Open Planning Project 2008
+ * @author Arne Kepp / The Open Planning Project 2009
+ * @author David Vick / Boundless 2017
+ *
+ * Original file
+ *
+ * SeedFormRestlet.java
+ */
 package org.geowebcache.rest.service;
 
 import com.google.common.base.Splitter;
