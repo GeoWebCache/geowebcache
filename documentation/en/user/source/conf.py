8da75cd131130fbc3284b28466cd13358e5386b9
Andrea Aime <andrea.aime@gmail.com>
2017-10-21 15:27:31 +0200

    Revert "Updated version to 1.11.2"
    This reverts commit a9055d41ae39d4774e06cfa8c732405bbf228895.

--- documentation/en/user/source/conf.py
+++ documentation/en/user/source/conf.py
@@ -47,7 +47,7 @@ copyright = u'OpenGeo, License: Creative Commons 3.0 - Attribution Share Alike'
 # The short X.Y version.
 version = '1.11'
 # The full version, including alpha/beta/rc tags.
-release = '1.11.2'
+release = '1.11.x'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
