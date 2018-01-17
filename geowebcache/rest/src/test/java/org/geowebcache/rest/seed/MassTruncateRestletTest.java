69d182a60b9f00be506a701ba373192ae0020d25
Andrea Aime <andrea.aime@gmail.com>
2018-01-17 19:12:40 +0100

    Mass truncate layer misleading 500 response when nothing is cached #587

--- geowebcache/rest/src/test/java/org/geowebcache/rest/seed/MassTruncateRestletTest.java
+++ geowebcache/rest/src/test/java/org/geowebcache/rest/seed/MassTruncateRestletTest.java
@@ -1,14 +1,26 @@
 package org.geowebcache.rest.seed;
 
+import static org.easymock.EasyMock.contains;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.*;
-import static org.easymock.classextension.EasyMock.*;
 
 import java.io.InputStream;
 
+import org.geowebcache.GeoWebCacheException;
+import org.geowebcache.config.Configuration;
 import org.geowebcache.config.XMLConfiguration;
 import org.geowebcache.config.XMLConfigurationBackwardsCompatibilityTest;
 import org.geowebcache.grid.GridSetBroker;
+import org.geowebcache.layer.AbstractTileLayer;
+import org.geowebcache.layer.TileLayer;
+import org.geowebcache.layer.wms.WMSLayer;
+import org.geowebcache.seed.TileBreeder;
 import org.geowebcache.storage.StorageBroker;
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.restlet.data.MediaType;
@@ -55,6 +67,71 @@ public class MassTruncateRestletTest {
 
         mtr.handle(request, response);
         
+        verify(sb);
+    }
+
+    @Test
+    public void testTruncateLayerTwice() throws Exception {
+        String layerName = "test";
+        String requestBody = "<truncateLayer><layerName>"+layerName+"</layerName></truncateLayer>";
+
+        StorageBroker sb = createMock(StorageBroker.class);
+        expect(sb.delete(eq(layerName))).andReturn(true);
+        expect(sb.delete(eq(layerName))).andReturn(false);
+        replay(sb);
+
+        Configuration config = createMock(Configuration.class);
+        TileLayer tileLayer = new TestTileLayer();
+        expect(config.getTileLayer(layerName)).andReturn(tileLayer); 
+        replay(config);
+
+        mtr.setStorageBroker(sb);
+        mtr.setConfiguration(config);
+
+        // first run, it will get an ok
+        Request request = new Request();
+        request.setMethod(Method.POST);
+        Representation entity = new StringRepresentation(requestBody, MediaType.TEXT_XML);
+        request.setEntity(entity);
+        Response response = new Response(request);
+
+        // first run, will delete from storage without issues
+        mtr.handle(request, response);
+        assertEquals(200, response.getStatus().getCode());
+        // second run, will get a falso and will check the breeded for layer existence
+        mtr.handle(request, response);
+        assertEquals(200, response.getStatus().getCode());
+        verify(sb);
+    }
+
+    @Test
+    public void testTruncateNonExistingLayer() throws Exception {
+        String layerName = "test";
+        String requestBody = "<truncateLayer><layerName>"+layerName+"</layerName></truncateLayer>";
+
+        StorageBroker sb = createMock(StorageBroker.class);
+        expect(sb.delete(eq(layerName))).andReturn(false);
+        replay(sb);
+
+        Configuration config = createMock(Configuration.class);
+        expect(config.getTileLayer(layerName)).andReturn(null);
+        replay(config);
+
+        mtr.setStorageBroker(sb);
+        mtr.setConfiguration(config);
+
+        // first run, it will get an ok
+        Request request = new Request();
+        request.setMethod(Method.POST);
+        Representation entity = new StringRepresentation(requestBody, MediaType.TEXT_XML);
+        request.setEntity(entity);
+        Response response = new Response(request);
+
+        // first run, will delete from storage without issues
+        mtr.handle(request, response);
+        assertEquals(400, response.getStatus().getCode());
+        assertThat(((StringRepresentation) response.getEntity()).getText(), 
+                CoreMatchers.containsString("Could not find layer test"));
         verify(sb);
     }
     
