158957b134633c4ece9c1d688e85019a87a9f4fb
Kevin Smith <ksmith@boundlessgeo.com>
2018-02-22 10:37:51 -0800

    Updated version to 1.14-SNAPSHOT

--- documentation/en/user/source/conf.py
+++ documentation/en/user/source/conf.py
@@ -45,9 +45,9 @@ copyright = u'OpenGeo, License: Creative Commons 3.0 - Attribution Share Alike'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.13'
+version = '1.14'
 # The full version, including alpha/beta/rc tags.
-release = '1.13.x'
+release = '1.14.x'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
