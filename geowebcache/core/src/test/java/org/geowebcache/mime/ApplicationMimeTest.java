9b93799801ae1f630e311362e3496fdce558d3d0
Andrea Aime <andrea.aime@gmail.com>
2018-10-10 16:19:43 +0200

    Adding support for mapbox vector tiles new format, retaining ability to lookup the old one

--- geowebcache/core/src/test/java/org/geowebcache/mime/ApplicationMimeTest.java
+++ geowebcache/core/src/test/java/org/geowebcache/mime/ApplicationMimeTest.java
@@ -7,38 +7,51 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(Parameterized.class)
+@RunWith(Enclosed.class)
 public class ApplicationMimeTest {
 
-    @Parameters(name = "{index}: {0}")
-    public static Collection<Object[]> data() {
-        List<Object[]> data = new ArrayList<>();
-        for (MimeType mt : ApplicationMime.ALL) {
-            data.add(new Object[] {mt.getFormat(), mt});
+    @RunWith(Parameterized.class)
+    public static class ParametricApplicationMimeTest {
+
+        @Parameters(name = "{index}: {0}")
+        public static Collection<Object[]> data() {
+            List<Object[]> data = new ArrayList<>();
+            for (MimeType mt : ApplicationMime.ALL) {
+                data.add(new Object[] {mt.getFormat(), mt});
+            }
+            return data;
         }
-        return data;
+
+        private MimeType mimeType;
+
+        public ParametricApplicationMimeTest(String format, MimeType mt) {
+            this.mimeType = mt;
+        }
+
+        @Test
+        public void testMimeType() throws MimeException {
+            MimeType result = MimeType.createFromFormat(mimeType.getFormat());
+            // Ensure it is not null
+            assertNotNull(result);
+            // Ensure it is the same instance
+            assertEquals(mimeType, result);
+            // get The MimeType from Extension
+            result = MimeType.createFromExtension(mimeType.getFileExtension());
+            // Ensure it is the same instance
+            assertEquals(mimeType, result);
+        }
     }
 
-    private MimeType mimeType;
-
-    public ApplicationMimeTest(String format, MimeType mt) {
-        this.mimeType = mt;
-    }
-
-    @Test
-    public void testMimeType() throws MimeException {
-        MimeType result = MimeType.createFromFormat(mimeType.getFormat());
-        // Ensure it is not null
-        assertNotNull(result);
-        // Ensure it is the same instance
-        assertEquals(mimeType, result);
-        // get The MimeType from Extension
-        result = MimeType.createFromExtension(mimeType.getFileExtension());
-        // Ensure it is the same instance
-        assertEquals(mimeType, result);
+    public static class OneOffApplicationMimeTest {
+        @Test
+        public void testMapboxLegacyFormat() throws MimeException {
+            MimeType mime = MimeType.createFromFormat(ApplicationMime.MAPBOX_TILES_LEGACY_MIME);
+            assertNotNull(mime);
+        }
     }
 }
