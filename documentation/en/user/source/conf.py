b83aefb8ffa4f7c3bc3ec22408d0ad8332cd899a
Andrea Aime <andrea.aime@gmail.com>
2019-01-25 19:32:49 +0100

    Revert "Updated version to 1.15-RC"
    This reverts commit 56389371f6d92b5493100b1c519f05a5037bc1b0.

--- documentation/en/user/source/conf.py
+++ documentation/en/user/source/conf.py
@@ -45,9 +45,9 @@ copyright = u'OpenGeo, License: Creative Commons 3.0 - Attribution Share Alike'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.15'
+version = '1.14'
 # The full version, including alpha/beta/rc tags.
-release = '1.15-RC'
+release = '1.14.x'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
