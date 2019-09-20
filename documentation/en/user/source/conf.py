c23d67bc8a15b43bc9a6e6d36f3ff9e24e7931f7
Alessandro Parma <alessa.parma@gmail.com>
2019-09-20 11:40:52 +0200

    Updated version to 1.15.3

--- documentation/en/user/source/conf.py
+++ documentation/en/user/source/conf.py
@@ -47,7 +47,7 @@ copyright = u'OpenGeo, License: Creative Commons 3.0 - Attribution Share Alike'
 # The short X.Y version.
 version = '1.15'
 # The full version, including alpha/beta/rc tags.
-release = '1.15.1'
+release = '1.15.3'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
