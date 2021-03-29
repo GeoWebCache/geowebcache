532b41568a3623b8f5fad09b41d84f9939d9cb0d
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2021-03-29 13:06:40 +0200

    Ticket-935-mbtiles: Supporting MBTiles Layer

---
+++ geowebcache/mbtiles/src/main/java/org/geowebcache/mbtiles/layer/MBTilesLayer.java
@@ -0,0 +1,355 @@
+/**
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU Lesser General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * <p>Copyright 2021
+ */
+package org.geowebcache.mbtiles.layer;
+
+import com.google.common.annotations.VisibleForTesting;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.InflaterInputStream;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geotools.mbtiles.MBTilesFile;
+import org.geotools.mbtiles.MBTilesMetadata;
+import org.geotools.mbtiles.MBTilesTile;
+import org.geowebcache.GeoWebCacheException;
+import org.geowebcache.config.DefaultGridsets;
+import org.geowebcache.conveyor.Conveyor.CacheResult;
+import org.geowebcache.conveyor.ConveyorTile;
+import org.geowebcache.grid.BoundingBox;
+import org.geowebcache.grid.GridSet;
+import org.geowebcache.grid.GridSetBroker;
+import org.geowebcache.grid.GridSubset;
+import org.geowebcache.grid.GridSubsetFactory;
+import org.geowebcache.grid.OutsideCoverageException;
+import org.geowebcache.io.ByteArrayResource;
+import org.geowebcache.layer.AbstractTileLayer;
+import org.geowebcache.layer.ExpirationRule;
+import org.geowebcache.mime.ApplicationMime;
+import org.geowebcache.mime.ImageMime;
+import org.geowebcache.mime.MimeType;
+import org.geowebcache.util.GWCVars;
+
+/**
+ * {@link org.geowebcache.layer.TileLayer} implementation for MBtiles layers
+ *
+ * @author Daniele Romagnoli
+ */
+public class MBTilesLayer extends AbstractTileLayer {
+
+    private static final String UNZIP_CONTENT_KEY = "gwc.mbtiles.pbf.unzip.debug";
+
+    private static final boolean UNZIP_CONTENT =
+            Boolean.valueOf(System.getProperty(UNZIP_CONTENT_KEY, "true"));
+
+    private static final int TILE_SIZE_256 = 256;
+
+    private static final int TILE_SIZE_512 = 512;
+
+    private static final int DEFAULT_TILE_SIZE = TILE_SIZE_256;
+
+    private static final Log LOG = LogFactory.getLog(MBTilesLayer.class);
+
+    /*
+     * configuration properties
+     */
+    private Boolean enabled;
+
+    private File tilesPath;
+
+    private int tileSize = DEFAULT_TILE_SIZE;
+
+    private transient MBTilesInfo tilesInfo;
+
+    private transient BoundingBox layerBounds;
+
+    private MimeType mimeType;
+
+    private MBTilesFile mbTilesFile;
+
+    private boolean vectorTiles;
+
+    @VisibleForTesting
+    MBTilesLayer(String name) {
+        this.name = name;
+    }
+
+    /** @return {@code null}, this kind of layer handles its own storage. */
+    @Override
+    public String getBlobStoreId() {
+        return null;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    /** Returns the location of the actual tiles set. */
+    public File getTilesPath() {
+        return tilesPath;
+    }
+
+    public void setTilesPath(File tilesPath) {
+        this.tilesPath = tilesPath;
+    }
+
+    public int getTileSize() {
+        return tileSize;
+    }
+
+    public void setTileSize(int tileSize) {
+        this.tileSize = tileSize;
+    }
+
+    public boolean isVectorTiles() {
+        return vectorTiles;
+    }
+
+    /**
+     * @return {@code true} if success. Note this method's return type should be void. It's not
+     *     checked anywhere
+     * @see org.geowebcache.layer.TileLayer#initialize(org.geowebcache.grid.GridSetBroker)
+     */
+    @Override
+    protected boolean initializeInternal(GridSetBroker gridSetBroker) {
+        if (this.enabled == null) {
+            this.enabled = true;
+        }
+        String specifiedName = getName();
+        if (tilesPath != null) {
+            if (!tilesPath.exists() || !tilesPath.canRead()) {
+                throw new IllegalStateException(
+                        "tilesPath property for this layer "
+                                + (specifiedName != null ? specifiedName : "")
+                                + " is set to '"
+                                + tilesPath
+                                + "' but the file either does not exist or is not readable");
+            }
+        }
+        try {
+            mbTilesFile = new MBTilesFile(tilesPath);
+            tilesInfo = new MBTilesInfo(mbTilesFile);
+            layerBounds = tilesInfo.getBounds();
+            if (StringUtils.isEmpty(specifiedName)) {
+                name = tilesInfo.getMetadataName();
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to open the Provided MBTile: " + tilesPath);
+        }
+
+        super.subSets = createGridSubsets(gridSetBroker);
+        super.formats = loadMimeTypes();
+        return true;
+    }
+
+    private List<MimeType> loadMimeTypes() {
+        MBTilesMetadata.t_format metadataFormat = tilesInfo.getFormat();
+
+        switch (metadataFormat) {
+            case PNG:
+                mimeType = ImageMime.png;
+                break;
+            case JPEG:
+            case JPG:
+                mimeType = ImageMime.jpeg;
+                break;
+            case PBF:
+                mimeType = ApplicationMime.mapboxVector;
+                vectorTiles = true;
+                break;
+        }
+        return Collections.singletonList(mimeType);
+    }
+
+    private HashMap<String, GridSubset> createGridSubsets(final GridSetBroker gridSetBroker) {
+        GridSet gridSet;
+
+        DefaultGridsets defaultGridSet = new DefaultGridsets(true, true);
+
+        if (tileSize <= 0) {
+            tileSize = DEFAULT_TILE_SIZE;
+        }
+
+        switch (tileSize) {
+            case TILE_SIZE_256:
+                gridSet = defaultGridSet.worldEpsg3857();
+                break;
+            case TILE_SIZE_512:
+                gridSet = defaultGridSet.worldEpsg3857x2();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported tileSize: " + tileSize);
+        }
+        Integer minZoom = tilesInfo.getMinZoom();
+        Integer maxZoom = tilesInfo.getMaxZoom();
+
+        GridSubset subSet =
+                GridSubsetFactory.createGridSubSet(gridSet, this.layerBounds, minZoom, maxZoom);
+
+        HashMap<String, GridSubset> subsets = new HashMap<>();
+        subsets.put(gridSet.getName(), subSet);
+        return subsets;
+    }
+
+    /** @see org.geowebcache.layer.TileLayer#getTile(org.geowebcache.conveyor.ConveyorTile) */
+    @Override
+    public ConveyorTile getTile(final ConveyorTile tile)
+            throws IOException, OutsideCoverageException {
+
+        long[] tileIndex = tile.getTileIndex();
+        int zl = (int) tileIndex[2];
+        int row = (int) tileIndex[1];
+        int column = (int) tileIndex[0];
+        MBTilesTile loadedTile = mbTilesFile.loadTile(zl, column, row);
+        byte[] content = loadedTile.getData();
+        if (content != null) {
+            if (tilesInfo.getFormat() == MBTilesMetadata.t_format.PBF) {
+                content = getPbfFromTile(content);
+            }
+
+            tile.setBlob(new ByteArrayResource(content));
+            tile.setCacheResult(CacheResult.HIT);
+        } else {
+            tile.setCacheResult(CacheResult.MISS);
+            throw new OutsideCoverageException(tile.getTileIndex(), 0, 0);
+        }
+
+        saveExpirationInformation((int) (tile.getExpiresHeader() / 1000));
+
+        return tile;
+    }
+
+    private byte[] getPbfFromTile(byte[] raw) throws IOException {
+        if (!UNZIP_CONTENT) {
+            return raw;
+        }
+        // GZIP magic number check
+        byte[] byteArray = raw;
+        if (raw != null && raw.length >= 2 && raw[0] == (byte) 0x1F && raw[1] == (byte) 0x8b) {
+            try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    ByteArrayInputStream bin = new ByteArrayInputStream(raw);
+                    InflaterInputStream in = new GZIPInputStream(bin)) {
+                byte[] buffer = new byte[1024];
+                int noRead;
+                while ((noRead = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, noRead);
+                }
+                byteArray = out.toByteArray();
+            }
+        }
+        return byteArray;
+    }
+
+    protected void saveExpirationInformation(int backendExpire) {
+        this.saveExpirationHeaders = false;
+
+        try {
+            if (getExpireCache(0) == GWCVars.CACHE_USE_WMS_BACKEND_VALUE) {
+                if (backendExpire == -1) {
+                    this.expireCacheList.set(0, new ExpirationRule(0, 7200));
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug(
+                                "Layer profile wants MaxAge from backend,"
+                                        + " but backend does not provide this. Setting to 7200 seconds.");
+                    }
+                } else {
+                    this.expireCacheList.set(backendExpire, new ExpirationRule(0, 7200));
+                }
+                if (LOG.isTraceEnabled()) {
+                    LOG.trace("Setting expireCache to: " + expireCache);
+                }
+            }
+            if (getExpireCache(0) == GWCVars.CACHE_USE_WMS_BACKEND_VALUE) {
+                if (backendExpire == -1) {
+                    this.expireClientsList.set(0, new ExpirationRule(0, 7200));
+                    if (LOG.isDebugEnabled()) {
+                        LOG.error(
+                                "Layer profile wants MaxAge from backend,"
+                                        + " but backend does not provide this. Setting to 7200 seconds.");
+                    }
+                } else {
+                    this.expireClientsList.set(0, new ExpirationRule(0, backendExpire));
+                    if (LOG.isTraceEnabled()) {
+                        LOG.trace("Setting expireClients to: " + expireClients);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // Sometimes this doesn't work (network conditions?),
+            // and it's really not worth getting caught up on it.
+            if (LOG.isDebugEnabled()) {
+                LOG.debug(e);
+            }
+        }
+    }
+
+    /**
+     * @see org.geowebcache.layer.TileLayer#getNoncachedTile(org.geowebcache.conveyor.ConveyorTile)
+     */
+    @Override
+    public ConveyorTile getNoncachedTile(ConveyorTile tile) throws GeoWebCacheException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @see org.geowebcache.layer.TileLayer#seedTile(org.geowebcache.conveyor.ConveyorTile, boolean)
+     */
+    @Override
+    public void seedTile(ConveyorTile tile, boolean tryCache)
+            throws GeoWebCacheException, IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @see
+     *     org.geowebcache.layer.TileLayer#doNonMetatilingRequest(org.geowebcache.conveyor.ConveyorTile)
+     */
+    @Override
+    public ConveyorTile doNonMetatilingRequest(ConveyorTile tile) throws GeoWebCacheException {
+        throw new UnsupportedOperationException();
+    }
+
+    /** @see org.geowebcache.layer.TileLayer#getStyles() */
+    @Override
+    public String getStyles() {
+        return null;
+    }
+
+    /**
+     * @see
+     *     org.geowebcache.layer.TileLayer#setExpirationHeader(javax.servlet.http.HttpServletResponse,
+     *     int)
+     */
+    @Override
+    public void setExpirationHeader(HttpServletResponse response, int zoomLevel) {
+        /*
+         * NOTE: this method doesn't seem like belonging to TileLayer, but to GeoWebCacheDispatcher
+         * itself
+         */
+    }
+}
---
+++ geowebcache/mbtiles/src/test/java/org/geowebcache/mbtiles/layer/MBTilesLayerTest.java
@@ -0,0 +1,188 @@
+/**
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU Lesser General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * <p>Copyright 2021
+ */
+package org.geowebcache.mbtiles.layer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.io.ByteStreams;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import no.ecc.vectortile.VectorTileDecoder;
+import org.geowebcache.GeoWebCacheException;
+import org.geowebcache.MockWepAppContextRule;
+import org.geowebcache.config.TestXMLConfigurationSource;
+import org.geowebcache.config.TileLayerConfiguration;
+import org.geowebcache.config.XMLConfigurationProvider;
+import org.geowebcache.conveyor.ConveyorTile;
+import org.geowebcache.grid.GridSubset;
+import org.geowebcache.layer.TileLayer;
+import org.geowebcache.layer.meta.LayerMetaInformation;
+import org.geowebcache.mime.ApplicationMime;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class MBTilesLayerTest {
+
+    private static final String TEST_POINTS_FILENAME = "manypoints_test.mbtiles";
+
+    public @Rule MockWepAppContextRule extensions = new MockWepAppContextRule();
+    protected TileLayerConfiguration config;
+
+    @Before
+    public void setUpTestUnit() throws Exception {
+        config = getConfig();
+    }
+
+    @Rule public TemporaryFolder temp = new TemporaryFolder();
+    protected File configDir;
+    protected File configFile;
+
+    protected TileLayerConfiguration getConfig() throws Exception {
+        makeConfigFile();
+        return getConfig(extensions);
+    }
+
+    private void makeConfigFile() throws Exception {
+        if (configFile == null) {
+            configDir = temp.getRoot();
+            configFile = temp.newFile("geowebcache.xml");
+
+            File lakesFile = temp.newFile("world_lakes.mbtiles");
+            URL source = MBTilesLayerTest.class.getResource("world_lakes.mbtiles");
+            Files.copy(
+                    Paths.get(source.toURI()),
+                    lakesFile.toPath(),
+                    StandardCopyOption.REPLACE_EXISTING);
+
+            File pointsFile = temp.newFile(TEST_POINTS_FILENAME);
+            source = MBTilesLayerTest.class.getResource(TEST_POINTS_FILENAME);
+            Files.copy(
+                    Paths.get(source.toURI()),
+                    pointsFile.toPath(),
+                    StandardCopyOption.REPLACE_EXISTING);
+
+            source = MBTilesLayerTest.class.getResource("geowebcache.xml");
+            try (Stream<String> lines = Files.lines(Paths.get(source.toURI()))) {
+                List<String> replaced =
+                        lines.map(
+                                        line -> {
+                                            return line.replace(
+                                                            "world_lakes.mbtiles",
+                                                            lakesFile.getAbsolutePath())
+                                                    .replace(
+                                                            TEST_POINTS_FILENAME,
+                                                            pointsFile.getAbsolutePath())
+                                                    .replace("\\", "\\\\");
+                                        })
+                                .collect(Collectors.toList());
+                Files.write(configFile.toPath(), replaced);
+            }
+        }
+    }
+
+    TestXMLConfigurationSource configSource = new TestXMLConfigurationSource();
+
+    private TileLayerConfiguration getConfig(MockWepAppContextRule extensions) throws Exception {
+        extensions.addBean(
+                "MBTilesLayerConfigProvider",
+                new MBTilesLayerXMLConfigurationProvider(),
+                XMLConfigurationProvider.class);
+        return configSource.create(extensions, configDir);
+    }
+
+    @Test
+    public void testLayerDefaultValues() {
+        Optional<TileLayer> layer = config.getLayer("World_Lakes");
+        TileLayer worldLakesLayer = layer.get();
+        assertTrue(worldLakesLayer instanceof MBTilesLayer);
+        MBTilesLayer mbTilesLayer = (MBTilesLayer) worldLakesLayer;
+        assertEquals(256, mbTilesLayer.getTileSize());
+    }
+
+    @Test
+    public void testLayerConfigValues() throws GeoWebCacheException, IOException {
+        Optional<TileLayer> layer = config.getLayer("testName");
+        TileLayer testLayer = layer.get();
+        assertTrue(testLayer instanceof MBTilesLayer);
+        MBTilesLayer mbTilesLayer = (MBTilesLayer) testLayer;
+        assertEquals(256, mbTilesLayer.getTileSize());
+        LayerMetaInformation metaInformation = mbTilesLayer.getMetaInformation();
+        assertNotNull(metaInformation);
+        assertEquals("TestDescription", metaInformation.getDescription());
+        assertEquals("TestTitle", metaInformation.getTitle());
+        Set<String> gridsubsets = mbTilesLayer.getGridSubsets();
+        gridsubsets.contains("EPSG:900913");
+        GridSubset subset = mbTilesLayer.getGridSubset("EPSG:900913");
+        assertEquals(6, subset.getZoomStop());
+        assertEquals(0, subset.getZoomStart());
+        assertEquals(ApplicationMime.mapboxVector, mbTilesLayer.getMimeTypes().get(0));
+
+        ConveyorTile conveyorTile =
+                new ConveyorTile(
+                        null,
+                        testLayer.getId(),
+                        subset.getName(),
+                        new long[] {4L, 4L, 3L},
+                        ApplicationMime.mapboxVector,
+                        null,
+                        null,
+                        null);
+
+        conveyorTile = testLayer.getTile(conveyorTile);
+        VectorTileDecoder decoder = new VectorTileDecoder();
+        byte[] byteArray = null;
+        try (InputStream inputStream = conveyorTile.getBlob().getInputStream()) {
+            byteArray = ByteStreams.toByteArray(inputStream);
+        }
+
+        VectorTileDecoder.FeatureIterable decoded = decoder.decode(byteArray);
+        List<VectorTileDecoder.Feature> features = decoded.asList();
+        assertEquals(433, features.size());
+        VectorTileDecoder.Feature feature = features.get(0);
+        assertEquals("manypoints_test", feature.getLayerName());
+        Map<String, Object> attribs = feature.getAttributes();
+        Set<String> keys = attribs.keySet();
+        Set<String> expectedKeys =
+                new HashSet<>(
+                        Arrays.asList(
+                                "_zoom", "_row", "_col", "ID", "Costant", "Zeroval", "IntVal",
+                                "Double1", "Double2", "Double3"));
+        expectedKeys.removeAll(keys);
+        assertTrue(expectedKeys.isEmpty());
+        assertEquals(3L, attribs.get("_zoom"));
+        assertEquals(4L, attribs.get("_row"));
+        assertEquals(4L, attribs.get("_col"));
+        assertEquals(25180626L, attribs.get("ID"));
+        assertEquals(37L, attribs.get("IntVal"));
+        assertEquals(3.25f, (Float) attribs.get("Double3"), 1e-6);
+    }
+}
