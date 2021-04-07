bc55215fb22adbcd6f76e963975fb5e89d2d6a50
Andrea Aime <andrea.aime@gmail.com>
2021-04-07 12:56:30 +0200

    Improve logging in lock providers

--- geowebcache/core/src/main/java/org/geowebcache/locks/NIOLockProvider.java
+++ geowebcache/core/src/main/java/org/geowebcache/locks/NIOLockProvider.java
@@ -163,6 +163,8 @@ public class NIOLockProvider implements LockProvider {
                                     LOGGER.debug(
                                             "Lock "
                                                     + lockKey
+                                                    + " on file "
+                                                    + lockFile
                                                     + " released by thread "
                                                     + Thread.currentThread().getId());
                                 }
