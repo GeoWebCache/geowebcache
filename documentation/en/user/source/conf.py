15ecbcd01bc203bfd020a6179ad37fb43c4d3a99
Andrea Aime <andrea.aime@gmail.com>
2023-05-05 07:57:11 +0000

    Revert "Updated version to 1.22.2"
    This reverts commit 7f2cc58279e2f8ee73f5e5ca04897850e8e004f6.

--- documentation/en/user/source/conf.py
+++ documentation/en/user/source/conf.py
@@ -47,7 +47,7 @@ copyright = u'OpenGeo, License: Creative Commons 3.0 - Attribution Share Alike'
 # The short X.Y version.
 version = '1.22'
 # The full version, including alpha/beta/rc tags.
-release = '1.22.2'
+release = '1.22-SNAPSHOT'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
