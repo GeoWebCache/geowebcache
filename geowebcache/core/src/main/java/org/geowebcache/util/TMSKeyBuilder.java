GeoWebCache/geowebcache

commit a133c4469dfde1ae58a6bdfc946305d7fe3fb7fe

Limit maximum code indentation, avoid long blocks flattened on the right side

geowebcache/core/src/main/java/org/geowebcache/util/TMSKeyBuilder.java
@@ -12,11 +12,12 @@
  *
  * @author Andrea Aime, GeoSolutions, Copyright 2019
  */
-package org.geowebcache.azure;
+package org.geowebcache.util;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.common.base.Strings;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringJoiner;
@@ -30,8 +31,8 @@ import org.geowebcache.mime.MimeType;
 import org.geowebcache.storage.TileObject;
 import org.geowebcache.storage.TileRange;
 
-// TODO: move to core?
-final class TMSKeyBuilder {
+/** Helper allowing to build TMS paths, commonly used in blob storage */
+public final class TMSKeyBuilder {
 
     private static final String DELIMITER = "/";
 
@@ -155,22 +156,14 @@ final class TMSKeyBuilder {
         String layerId = layerId(layerName);
         // Coordinates prefix: {@code <prefix>/<layer name>/<gridset id>/<format id>/<parameters
         // hash>/}
-        return layerGridsets(layerName)
-                .stream()
-                .flatMap(
-                        gridsetId ->
-                                layerFormats(layerName)
-                                        .stream()
-                                        .map(
-                                                format ->
-                                                        join(
-                                                                true,
-                                                                prefix,
-                                                                layerId,
-                                                                gridsetId,
-                                                                format,
-                                                                parametersId)))
-                .collect(Collectors.toSet());
+        Set<String> set = new HashSet<>();
+        for (String gridsetId : layerGridsets(layerName)) {
+            for (String format : layerFormats(layerName)) {
+                String join = join(true, prefix, layerId, gridsetId, format, parametersId);
+                set.add(join);
+            }
+        }
+        return set;
     }
 
     public String layerMetadata(final String layerName) {
@@ -202,7 +195,7 @@ final class TMSKeyBuilder {
      * @return the key prefix up to the coordinates (i.e. {@code
      *     "<prefix>/<layer>/<gridset>/<format>/<parametersId>"})
      */
-    public String coordinatesPrefix(TileRange obj) {
+    public String coordinatesPrefix(TileRange obj, boolean endWithSlah) {
         checkNotNull(obj.getLayerName());
         checkNotNull(obj.getGridSetId());
         checkNotNull(obj.getMimeType());
@@ -223,7 +216,7 @@ final class TMSKeyBuilder {
         }
         String shortFormat = mimeType.getFileExtension(); // png, png8, png24, etc
 
-        String key = join(false, prefix, layer, gridset, shortFormat, parametersId);
+        String key = join(endWithSlah, prefix, layer, gridset, shortFormat, parametersId);
         return key;
     }
 

geowebcache/s3storage/src/main/java/org/geowebcache/s3/S3BlobStore.java
@@ -69,6 +69,7 @@ import org.geowebcache.storage.StorageException;
 import org.geowebcache.storage.TileObject;
 import org.geowebcache.storage.TileRange;
 import org.geowebcache.storage.TileRangeIterator;
+import org.geowebcache.util.TMSKeyBuilder;
 
 public class S3BlobStore implements BlobStore {
 
@@ -309,7 +310,7 @@ public class S3BlobStore implements BlobStore {
     @Override
     public boolean delete(final TileRange tileRange) throws StorageException {
 
-        final String coordsPrefix = keyBuilder.coordinatesPrefix(tileRange);
+        final String coordsPrefix = keyBuilder.coordinatesPrefix(tileRange, false);
         if (!s3Ops.prefixExists(coordsPrefix)) {
             return false;
         }

geowebcache/s3storage/src/main/java/org/geowebcache/s3/TMSKeyBuilder.java
@@ -1,237 +1,0 @@
-/**
- * This program is free software: you can redistribute it and/or modify it under the terms of the
- * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
- *
- * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * <p>You should have received a copy of the GNU Lesser General Public License along with this
- * program. If not, see <http://www.gnu.org/licenses/>.
- *
- * @author Gabriel Roldan, Boundless Spatial Inc, Copyright 2015
- */
-package org.geowebcache.s3;
-
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import com.google.common.base.Strings;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringJoiner;
-import java.util.stream.Collectors;
-import org.geowebcache.GeoWebCacheException;
-import org.geowebcache.filter.parameters.ParametersUtils;
-import org.geowebcache.layer.TileLayer;
-import org.geowebcache.layer.TileLayerDispatcher;
-import org.geowebcache.mime.MimeException;
-import org.geowebcache.mime.MimeType;
-import org.geowebcache.storage.TileObject;
-import org.geowebcache.storage.TileRange;
-
-final class TMSKeyBuilder {
-
-    private static final String DELIMITER = "/";
-
-    public static final String LAYER_METADATA_OBJECT_NAME = "metadata.properties";
-    public static final String PARAMETERS_METADATA_OBJECT_PREFIX = "parameters-";
-    public static final String PARAMETERS_METADATA_OBJECT_SUFFIX = ".properties";
-
-    private String prefix;
-
-    private TileLayerDispatcher layers;
-
-    public TMSKeyBuilder(final String prefix, TileLayerDispatcher layers) {
-        this.prefix = prefix;
-        this.layers = layers;
-    }
-
-    public String layerId(String layerName) {
-        TileLayer layer;
-        try {
-            layer = layers.getTileLayer(layerName);
-        } catch (GeoWebCacheException e) {
-            throw new RuntimeException(e);
-        }
-        return layer.getId();
-    }
-
-    public Set<String> layerGridsets(String layerName) {
-        TileLayer layer;
-        try {
-            layer = layers.getTileLayer(layerName);
-        } catch (GeoWebCacheException e) {
-            throw new RuntimeException(e);
-        }
-        return layer.getGridSubsets();
-    }
-
-    public Set<String> layerFormats(String layerName) {
-        TileLayer layer;
-        try {
-            layer = layers.getTileLayer(layerName);
-        } catch (GeoWebCacheException e) {
-            throw new RuntimeException(e);
-        }
-        return layer.getMimeTypes()
-                .stream()
-                .map(MimeType::getFileExtension)
-                .collect(Collectors.toSet());
-    }
-
-    public String forTile(TileObject obj) {
-        checkNotNull(obj.getLayerName());
-        checkNotNull(obj.getGridSetId());
-        checkNotNull(obj.getBlobFormat());
-        checkNotNull(obj.getXYZ());
-
-        String layer = layerId(obj.getLayerName());
-        String gridset = obj.getGridSetId();
-        String shortFormat;
-        String parametersId = obj.getParametersId();
-        if (parametersId == null) {
-            Map<String, String> parameters = obj.getParameters();
-            parametersId = ParametersUtils.getId(parameters);
-            if (parametersId == null) {
-                parametersId = "default";
-            } else {
-                obj.setParametersId(parametersId);
-            }
-        }
-        Long x = Long.valueOf(obj.getXYZ()[0]);
-        Long y = Long.valueOf(obj.getXYZ()[1]);
-        Long z = Long.valueOf(obj.getXYZ()[2]);
-        String extension;
-        try {
-            String format = obj.getBlobFormat();
-            MimeType mimeType = MimeType.createFromFormat(format);
-            shortFormat = mimeType.getFileExtension(); // png, png8, png24, etc
-            extension = mimeType.getInternalName(); // png, jpeg, etc
-        } catch (MimeException e) {
-            throw new RuntimeException(e);
-        }
-
-        // Key format, comprised of
-        // {@code <prefix>/<layer name>/<gridset id>/<format id>/<parameters
-        // hash>/<z>/<x>/<y>.<extension>}
-        String key =
-                join(
-                        false,
-                        prefix,
-                        layer,
-                        gridset,
-                        shortFormat,
-                        parametersId,
-                        z,
-                        x,
-                        y + "." + extension);
-        return key;
-    }
-
-    public String forLayer(final String layerName) {
-        String layerId = layerId(layerName);
-        // Layer prefix format, comprised of {@code <prefix>/<layer name>/}
-        return join(true, prefix, layerId);
-    }
-
-    public String forGridset(final String layerName, final String gridsetId) {
-        String layerId = layerId(layerName);
-        // Layer prefix format, comprised of {@code <prefix>/<layer name>/}
-        return join(true, prefix, layerId, gridsetId);
-    }
-
-    public Set<String> forParameters(final String layerName, final String parametersId) {
-        String layerId = layerId(layerName);
-        // Coordinates prefix: {@code <prefix>/<layer name>/<gridset id>/<format id>/<parameters
-        // hash>/}
-        return layerGridsets(layerName)
-                .stream()
-                .flatMap(
-                        gridsetId ->
-                                layerFormats(layerName)
-                                        .stream()
-                                        .map(
-                                                format ->
-                                                        join(
-                                                                true,
-                                                                prefix,
-                                                                layerId,
-                                                                gridsetId,
-                                                                format,
-                                                                parametersId)))
-                .collect(Collectors.toSet());
-    }
-
-    public String layerMetadata(final String layerName) {
-        String layerId = layerId(layerName);
-        return join(false, prefix, layerId, LAYER_METADATA_OBJECT_NAME);
-    }
-
-    public String storeMetadata() {
-        return join(false, prefix, LAYER_METADATA_OBJECT_NAME);
-    }
-
-    public String parametersMetadata(final String layerName, final String parametersId) {
-        String layerId = layerId(layerName);
-        return join(
-                false,
-                prefix,
-                layerId,
-                PARAMETERS_METADATA_OBJECT_PREFIX
-                        + parametersId
-                        + PARAMETERS_METADATA_OBJECT_SUFFIX);
-    }
-
-    public String parametersMetadataPrefix(final String layerName) {
-        String layerId = layerId(layerName);
-        return join(false, prefix, layerId, PARAMETERS_METADATA_OBJECT_PREFIX);
-    }
-
-    /**
-     * @return the key prefix up to the coordinates (i.e. {@code
-     *     "<prefix>/<layer>/<gridset>/<format>/<parametersId>"})
-     */
-    public String coordinatesPrefix(TileRange obj) {
-        checkNotNull(obj.getLayerName());
-        checkNotNull(obj.getGridSetId());
-        checkNotNull(obj.getMimeType());
-
-        String layer = layerId(obj.getLayerName());
-        String gridset = obj.getGridSetId();
-        MimeType mimeType = obj.getMimeType();
-
-        String parametersId = obj.getParametersId();
-        if (parametersId == null) {
-            Map<String, String> parameters = obj.getParameters();
-            parametersId = ParametersUtils.getId(parameters);
-            if (parametersId == null) {
-                parametersId = "default";
-            } else {
-                obj.setParametersId(parametersId);
-            }
-        }
-        String shortFormat = mimeType.getFileExtension(); // png, png8, png24, etc
-
-        String key = join(true, prefix, layer, gridset, shortFormat, parametersId);
-        return key;
-    }
-
-    public String pendingDeletes() {
-        return String.format("%s/%s", prefix, "_pending_deletes.properties");
-    }
-
-    private static String join(boolean closing, Object... elements) {
-        StringJoiner joiner = new StringJoiner(DELIMITER);
-        for (Object o : elements) {
-            String s = o == null ? null : o.toString();
-            if (!Strings.isNullOrEmpty(s)) {
-                joiner.add(s);
-            }
-        }
-        if (closing) {
-            joiner.add("");
-        }
-        return joiner.toString();
-    }
-}

geowebcache/swiftblob/src/main/java/org/geowebcache/swift/TMSKeyBuilder.java
@@ -1,235 +1,0 @@
-/**
- * This program is free software: you can redistribute it and/or modify it under the terms of the
- * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
- *
- * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * <p>You should have received a copy of the GNU Lesser General Public License along with this
- * program. If not, see <http://www.gnu.org/licenses/>.
- *
- * @author Gabriel Roldan, Boundless Spatial Inc, Copyright 2015
- */
-package org.geowebcache.swift;
-
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import com.google.common.base.Strings;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringJoiner;
-import java.util.stream.Collectors;
-import org.geowebcache.GeoWebCacheException;
-import org.geowebcache.filter.parameters.ParametersUtils;
-import org.geowebcache.layer.TileLayer;
-import org.geowebcache.layer.TileLayerDispatcher;
-import org.geowebcache.mime.MimeException;
-import org.geowebcache.mime.MimeType;
-import org.geowebcache.storage.TileObject;
-import org.geowebcache.storage.TileRange;
-
-/** Provides methods to retrieve path information for tile objects. */
-final class TMSKeyBuilder {
-
-    private static final String DELIMITER = "/";
-
-    public static final String LAYER_METADATA_OBJECT_NAME = "metadata.properties";
-    public static final String PARAMETERS_METADATA_OBJECT_PREFIX = "parameters-";
-    public static final String PARAMETERS_METADATA_OBJECT_SUFFIX = ".properties";
-
-    private final String prefix;
-
-    private final TileLayerDispatcher layers;
-
-    public TMSKeyBuilder(final String prefix, TileLayerDispatcher layers) {
-        this.prefix = prefix;
-        this.layers = layers;
-    }
-
-    public String layerId(String layerName) {
-        TileLayer layer;
-        try {
-            layer = layers.getTileLayer(layerName);
-        } catch (GeoWebCacheException e) {
-            throw new RuntimeException(e);
-        }
-        return layer.getId();
-    }
-
-    public Set<String> layerGridsets(String layerName) {
-        TileLayer layer;
-        try {
-            layer = layers.getTileLayer(layerName);
-        } catch (GeoWebCacheException e) {
-            throw new RuntimeException(e);
-        }
-        return layer.getGridSubsets();
-    }
-
-    public Set<String> layerFormats(String layerName) {
-        TileLayer layer;
-        try {
-            layer = layers.getTileLayer(layerName);
-        } catch (GeoWebCacheException e) {
-            throw new RuntimeException(e);
-        }
-        return layer.getMimeTypes()
-                .stream()
-                .map(MimeType::getFileExtension)
-                .collect(Collectors.toSet());
-    }
-
-    public String forTile(TileObject obj) {
-        checkNotNull(obj.getLayerName());
-        checkNotNull(obj.getGridSetId());
-        checkNotNull(obj.getBlobFormat());
-        checkNotNull(obj.getXYZ());
-
-        String layer = layerId(obj.getLayerName());
-        String gridset = obj.getGridSetId();
-        String shortFormat;
-        String parametersId = obj.getParametersId();
-        if (parametersId == null) {
-            Map<String, String> parameters = obj.getParameters();
-            parametersId = ParametersUtils.getId(parameters);
-            if (parametersId == null) {
-                parametersId = "default";
-            } else {
-                obj.setParametersId(parametersId);
-            }
-        }
-        Long x = Long.valueOf(obj.getXYZ()[0]);
-        Long y = Long.valueOf(obj.getXYZ()[1]);
-        Long z = Long.valueOf(obj.getXYZ()[2]);
-        String extension;
-        try {
-            String format = obj.getBlobFormat();
-            MimeType mimeType = MimeType.createFromFormat(format);
-            shortFormat = mimeType.getFileExtension(); // png, png8, png24, etc
-            extension = mimeType.getInternalName(); // png, jpeg, etc
-        } catch (MimeException e) {
-            throw new RuntimeException(e);
-        }
-
-        // Key format, comprised of
-        // {@code <prefix>/<layer name>/<gridset id>/<format id>/<parameters
-        // hash>/<z>/<x>/<y>.<extension>}
-        return join(
-                false,
-                prefix,
-                layer,
-                gridset,
-                shortFormat,
-                parametersId,
-                z,
-                x,
-                y + "." + extension);
-    }
-
-    public String forLayer(final String layerName) {
-        String layerId = layerId(layerName);
-        // Layer prefix format, comprised of {@code <prefix>/<layer name>/}
-        return join(true, prefix, layerId);
-    }
-
-    public String forGridset(final String layerName, final String gridsetId) {
-        String layerId = layerId(layerName);
-        // Layer prefix format, comprised of {@code <prefix>/<layer name>/}
-        return join(true, prefix, layerId, gridsetId);
-    }
-
-    public Set<String> forParameters(final String layerName, final String parametersId) {
-        String layerId = layerId(layerName);
-        // Coordinates prefix: {@code <prefix>/<layer name>/<gridset id>/<format id>/<parameters
-        // hash>/}
-        return layerGridsets(layerName)
-                .stream()
-                .flatMap(
-                        gridsetId ->
-                                layerFormats(layerName)
-                                        .stream()
-                                        .map(
-                                                format ->
-                                                        join(
-                                                                true,
-                                                                prefix,
-                                                                layerId,
-                                                                gridsetId,
-                                                                format,
-                                                                parametersId)))
-                .collect(Collectors.toSet());
-    }
-
-    public String layerMetadata(final String layerName) {
-        String layerId = layerId(layerName);
-        return join(false, prefix, layerId, LAYER_METADATA_OBJECT_NAME);
-    }
-
-    public String storeMetadata() {
-        return join(false, prefix, LAYER_METADATA_OBJECT_NAME);
-    }
-
-    public String parametersMetadata(final String layerName, final String parametersId) {
-        String layerId = layerId(layerName);
-        return join(
-                false,
-                prefix,
-                layerId,
-                PARAMETERS_METADATA_OBJECT_PREFIX
-                        + parametersId
-                        + PARAMETERS_METADATA_OBJECT_SUFFIX);
-    }
-
-    public String parametersMetadataPrefix(final String layerName) {
-        String layerId = layerId(layerName);
-        return join(false, prefix, layerId, PARAMETERS_METADATA_OBJECT_PREFIX);
-    }
-
-    /**
-     * @return the key prefix up to the coordinates (i.e. {@code
-     *     "<prefix>/<layer>/<gridset>/<format>/<parametersId>"})
-     */
-    public String coordinatesPrefix(TileRange obj) {
-        checkNotNull(obj.getLayerName());
-        checkNotNull(obj.getGridSetId());
-        checkNotNull(obj.getMimeType());
-
-        String layer = layerId(obj.getLayerName());
-        String gridset = obj.getGridSetId();
-        MimeType mimeType = obj.getMimeType();
-
-        String parametersId = obj.getParametersId();
-        if (parametersId == null) {
-            Map<String, String> parameters = obj.getParameters();
-            parametersId = ParametersUtils.getId(parameters);
-            if (parametersId == null) {
-                parametersId = "default";
-            } else {
-                obj.setParametersId(parametersId);
-            }
-        }
-        String shortFormat = mimeType.getFileExtension(); // png, png8, png24, etc
-
-        return join(true, prefix, layer, gridset, shortFormat, parametersId);
-    }
-
-    public String pendingDeletes() {
-        return String.format("%s/%s", prefix, "_pending_deletes.properties");
-    }
-
-    private static String join(boolean closing, Object... elements) {
-        StringJoiner joiner = new StringJoiner(DELIMITER);
-        for (Object o : elements) {
-            String s = o == null ? null : o.toString();
-            if (!Strings.isNullOrEmpty(s)) {
-                joiner.add(s);
-            }
-        }
-        if (closing) {
-            joiner.add("");
-        }
-        return joiner.toString();
-    }
-}

geowebcache/azureblob/src/main/java/org/geowebcache/azure/AzureBlobStore.java
@@ -57,6 +57,7 @@ import org.geowebcache.storage.StorageException;
 import org.geowebcache.storage.TileObject;
 import org.geowebcache.storage.TileRange;
 import org.geowebcache.storage.TileRangeIterator;
+import org.geowebcache.util.TMSKeyBuilder;
 import org.springframework.http.HttpStatus;
 
 public class AzureBlobStore implements BlobStore {
@@ -201,7 +202,7 @@ public class AzureBlobStore implements BlobStore {
     @Override
     public boolean delete(TileRange tileRange) throws StorageException {
         // see if there is anything to delete in that range by computing a prefix
-        final String coordsPrefix = keyBuilder.coordinatesPrefix(tileRange);
+        final String coordsPrefix = keyBuilder.coordinatesPrefix(tileRange, true);
         if (client.listBlobs(coordsPrefix, 1).isEmpty()) {
             return false;
         }

geowebcache/azureblob/src/main/java/org/geowebcache/azure/DeleteManager.java
@@ -42,6 +42,7 @@ import org.geowebcache.GeoWebCacheException;
 import org.geowebcache.locks.LockProvider;
 import org.geowebcache.locks.LockProvider.Lock;
 import org.geowebcache.storage.StorageException;
+import org.geowebcache.util.TMSKeyBuilder;
 import org.springframework.http.HttpStatus;
 
 /**

geowebcache/checkstyle.xml
@@ -34,4 +34,11 @@
 \*.*\d{4}.*$"/>
     <property name="fileExtensions" value="java"/>
   </module>
+    <module name="RegexpSingleline">
+    <property name="format" value="^\s{60}"/>
+    <property name="message" value="Excessive nesting found. Please try to factor out the deeply nested code in a separate method or class"/>
+    <property name="minimum" value="0"/>
+    <property name="maximum" value="1"/>
+    <property name="fileExtensions" value="java"/>
+  </module>
 </module>

geowebcache/core/src/main/java/org/geowebcache/demo/Demo.java
@@ -214,35 +214,7 @@ public class Demo {
                 buf.append("]</td><td>\n");
 
                 if (gridSubset.getName().equals(gridSetBroker.getWorldEpsg4326().getName())) {
-                    buf.append(" &nbsp; KML: [");
-                    String prefix = "";
-
-                    buf.append(
-                            layer.getMimeTypes()
-                                    .stream()
-                                    .filter(
-                                            type ->
-                                                    type instanceof ImageMime
-                                                            || type == XMLMime.kml
-                                                            || type == XMLMime.kmz)
-                                    .map(
-                                            type -> {
-                                                if (type == XMLMime.kmz) {
-                                                    return String.format(
-                                                            "<a href=\"%sservice/kml/%s.kml.kmz\">kmz</a>",
-                                                            prefix, layer.getName());
-                                                } else {
-                                                    return String.format(
-                                                            "<a href=\"%sservice/kml/%s.%s.kml\">%s</a>",
-                                                            prefix,
-                                                            layer.getName(),
-                                                            type.getFileExtension(),
-                                                            type.getFileExtension());
-                                                }
-                                            })
-                                    .collect(Collectors.joining(", ")));
-
-                    buf.append("]");
+                    outputKMLSupport(buf, layer);
                 }
                 buf.append("</td></tr>");
             }
@@ -250,6 +222,38 @@ public class Demo {
             buf.append("</table></td>\n");
             buf.append("</tr>\n");
         }
+    }
+
+    private static void outputKMLSupport(StringBuffer buf, TileLayer layer) {
+        buf.append(" &nbsp; KML: [");
+        String prefix = "";
+
+        buf.append(
+                layer.getMimeTypes()
+                        .stream()
+                        .filter(
+                                type ->
+                                        type instanceof ImageMime
+                                                || type == XMLMime.kml
+                                                || type == XMLMime.kmz)
+                        .map(
+                                type -> {
+                                    if (type == XMLMime.kmz) {
+                                        return String.format(
+                                                "<a href=\"%sservice/kml/%s.kml.kmz\">kmz</a>",
+                                                prefix, layer.getName());
+                                    } else {
+                                        return String.format(
+                                                "<a href=\"%sservice/kml/%s.%s.kml\">%s</a>",
+                                                prefix,
+                                                layer.getName(),
+                                                type.getFileExtension(),
+                                                type.getFileExtension());
+                                    }
+                                })
+                        .collect(Collectors.joining(", ")));
+
+        buf.append("]");
     }
 
     private static String generateDemoUrl(String layerName, String gridSetId, MimeType type) {

commit a1a70d33facd19445e16abab6f12b35995b62d41

#1157: Fix for S3BlobStore bulk delete (without listeners)
(failing since a133c446, test ignored since 855ed094)

geowebcache/core/src/main/java/org/geowebcache/util/TMSKeyBuilder.java
@@ -194,7 +194,7 @@ public final class TMSKeyBuilder {
      * @return the key prefix up to the coordinates (i.e. {@code
      *     "<prefix>/<layer>/<gridset>/<format>/<parametersId>"})
      */
-    public String coordinatesPrefix(TileRange obj, boolean endWithSlah) {
+    public String coordinatesPrefix(TileRange obj, boolean endWithSlash) {
         checkNotNull(obj.getLayerName());
         checkNotNull(obj.getGridSetId());
         checkNotNull(obj.getMimeType());
@@ -215,7 +215,7 @@ public final class TMSKeyBuilder {
         }
         String shortFormat = mimeType.getFileExtension(); // png, png8, png24, etc
 
-        String key = join(endWithSlah, prefix, layer, gridset, shortFormat, parametersId);
+        String key = join(endWithSlash, prefix, layer, gridset, shortFormat, parametersId);
         return key;
     }
 

geowebcache/s3storage/src/main/java/org/geowebcache/s3/S3BlobStore.java
@@ -311,7 +311,7 @@ public class S3BlobStore implements BlobStore {
     @Override
     public boolean delete(final TileRange tileRange) throws StorageException {
 
-        final String coordsPrefix = keyBuilder.coordinatesPrefix(tileRange, false);
+        final String coordsPrefix = keyBuilder.coordinatesPrefix(tileRange, true);
         if (!s3Ops.prefixExists(coordsPrefix)) {
             return false;
         }