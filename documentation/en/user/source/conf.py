a8a18b99a969220cb1dbfb41f275d78ca6a45648
petersmythe <github@smythe.co.za>
2024-09-18 09:09:25 +0000

    Updated version to 1.26.0

--- documentation/en/user/source/conf.py
+++ documentation/en/user/source/conf.py
@@ -47,7 +47,7 @@ copyright = u'OpenGeo, License: Creative Commons 3.0 - Attribution Share Alike'
 # The short X.Y version.
 version = '1.26'
 # The full version, including alpha/beta/rc tags.
-release = '1.26-SNAPSHOT'
+release = '1.26.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
