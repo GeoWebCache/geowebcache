d5b2338e5e5a32f5833d3dd237dcf1684bf31c1c
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2026-01-28 08:13:52 +0100

    Stick to Junit4 for now

--- geowebcache/rest/src/test/java/org/geowebcache/rest/controller/GridSetControllerTest.java
+++ geowebcache/rest/src/test/java/org/geowebcache/rest/controller/GridSetControllerTest.java
@@ -29,6 +29,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+@SuppressWarnings("deprecation")
 @RunWith(SpringRunner.class)
 @WebAppConfiguration()
 @ContextConfiguration({
