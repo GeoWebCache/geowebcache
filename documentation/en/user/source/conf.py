af66a113232c01eede8efdaa962e0b93dbf063f4
aaime <andrea.aime@gmail.com>
2026-04-17 13:23:55 +0000

    Revert "Updated version to 2.0-RC"
    This reverts commit 36dcb526c9664d96bbfeaa58e3226f858844816b.

--- documentation/en/user/source/conf.py
+++ documentation/en/user/source/conf.py
@@ -47,7 +47,7 @@ copyright = u'OpenGeo, License: Creative Commons 3.0 - Attribution Share Alike'
 # The short X.Y version.
 version = '2.0'
 # The full version, including alpha/beta/rc tags.
-release = '2.0-RC'
+release = '2.0-SNAPSHOT'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
